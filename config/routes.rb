674d048f48d76de83e3dca9f679ff83cab17451a
Max Mulatz <klappradla@gmail.com>
2015-03-13 16:05:37 +0100

    undo user scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
-Rails.application.routes.draw do
-  resources :users
-  root 'users#index'
+Rails.application.routes.draw do  root 'users#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
