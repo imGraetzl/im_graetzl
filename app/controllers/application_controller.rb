009d67d52435bc050c82b68214d23ae0a0116750
Max Mulatz <klappradla@gmail.com>
2015-03-16 16:36:14 +0100

    allow username for login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,15 @@ class ApplicationController < ActionController::Base
   # modify devise to allow additional user attributes
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u|
       u.permit(:username, :gender, :birthday, :first_name, :last_name, :email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.for(:sign_in) do |u|
+      u.permit(:login, :username, :email, :password, :remember_me)
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  # validations
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+
+  # virtual attribute to login with either username or password
+  attr_accessor :login
+
+  # overwrite devise authentication method to allow username OR email
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).
+        where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).
+        first
+    else
+      where(conditions.to_h).first
+    end
+  end
 end
