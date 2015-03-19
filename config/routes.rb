e39618dfb66436dbce7435485ef06c3edbe77a9d
Max Mulatz <klappradla@gmail.com>
2015-03-19 23:45:45 +0100

    inject ajax data

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: "registrations"}
 
   devise_scope :user do
-    get 'users/sign_up/address', to: 'registrations#address'
+    get "fetch_address" => 'registrations#address', as: 'fetch_address'
   end
 
   root 'static_pages#welcome'
