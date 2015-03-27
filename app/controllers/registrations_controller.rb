3b6b7518f201bfc863fdae70786bbbf97869dd5a
Max Mulatz <klappradla@gmail.com>
2015-03-27 16:31:11 +0100

    address nested attributes [skip ci]

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,21 @@
 class RegistrationsController < Devise::RegistrationsController
   
   # modify devise to allow additional user attributes
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters
 
+  def new
+    build_resource({})
+    self.resource.build_address
+    respond_with self.resource
+  end
 
   protected
+
+  # override build_resource to create nested resources
+  # def build_resource(hash = nil)
+  #   super
+  #   self.resource.build_address    
+  # end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u|
@@ -16,7 +27,8 @@ class RegistrationsController < Devise::RegistrationsController
         :email,
         :password, :password_confirmation,
         :terms_and_conditions,
-        :newsletter)
+        :newsletter,
+        address_attributes: [:street_name])
     end
     devise_parameter_sanitizer.for(:sign_in) do |u|
       u.permit(:login, :username, :email, :password, :remember_me)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_one :address, dependent: :destroy
+  accepts_nested_attributes_for :address
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
