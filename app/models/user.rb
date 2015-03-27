1b869d00193ea6f2a7d0c015388797f3a065326c
Max Mulatz <klappradla@gmail.com>
2015-03-27 14:50:32 +0100

    add registration fields and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,19 @@
 class User < ActiveRecord::Base
+  has_one :address, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  # virtual attribute to login with either username or password
+  # virtual attribute to login with username or email
   attr_accessor :login
 
-  has_one :address, dependent: :destroy
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 50 }
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+  validates :terms_and_conditions, acceptance: true
+
 
   # overwrite devise authentication method to allow username OR email
   def self.find_for_database_authentication(warden_conditions)
