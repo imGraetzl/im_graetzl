e3800e7eefb049e0e5c561adc83cd14600928426
Max Mulatz <klappradla@gmail.com>
2015-04-03 12:38:06 +0200

    basic feature specs registration :rocket:

--- spec/features/registrations_spec.rb
+++ spec/features/registrations_spec.rb
@@ -36,23 +36,43 @@ RSpec.feature "Registrations", type: :feature do
   end
 
   describe 'step 2: submit address' do
-    let(:address) { build(:esterhazygasse) }
-    it 'injects address values in hidden field', js: true do
-      fill_in 'address', with: "#{address.street_name} #{address.street_number}"
+    let(:esterhazygasse) { build(:esterhazygasse) }
+    let(:naschmarkt) { build(:naschmarkt) }
+
+    before do
+      fill_in 'address', with: "#{esterhazygasse.street_name} #{esterhazygasse.street_number}"
       click_button('Weiter')
       wait_for_ajax
+    end
 
+    it 'injects address_attributes in hidden field', js: true do
       street_name = find("input[name='user[address_attributes][street_name]']", visible: false).value
       street_number = find("input[name='user[address_attributes][street_number]']", visible: false).value
       zip = find("input[name='user[address_attributes][zip]']", visible: false).value
       city = find("input[name='user[address_attributes][city]']", visible: false).value
       coords = find("input[name='user[address_attributes][coordinates]']", visible: false).value
 
-      expect(street_name).to eq(address.street_name)
-      expect(street_number).to eq(address.street_number)
-      expect(zip).to eq(address.zip)
-      expect(city).to eq(address.city)
-      expect(coords).to eq(address.coordinates.as_text)
+      expect(street_name).to eq(esterhazygasse.street_name)
+      expect(street_number).to eq(esterhazygasse.street_number)
+      expect(zip).to eq(esterhazygasse.zip)
+      expect(city).to eq(esterhazygasse.city)
+      expect(coords).to eq(esterhazygasse.coordinates.as_text)
+    end
+    it 'has hidden field for graetzl_name', js: true do
+      graetzl_fields = find('div#user_graetzl_fields', visible: false)
+      within graetzl_fields do
+        expect(page).to have_selector("input[name='user[graetzl_attributes][name]']", visible: false)
+      end
+    end
+    it 'injects graetzl_name in hidden field', js: true do
+      graetzl_name = find("input[name='user[graetzl_attributes][name]']", visible: false).value
+      expect(graetzl_name).to eq(naschmarkt.name)
+    end
+    it 'shows welcome message', js: true do
+      fields_description = find('div#user_fields_description')
+      within fields_description do
+        expect(page).to have_content(naschmarkt.name)
+      end
     end
   end
 end
