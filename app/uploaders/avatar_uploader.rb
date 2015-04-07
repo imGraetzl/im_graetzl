63ee15ec324585fc7cf69aab41eaa44d30f929d6
Max Mulatz <klappradla@gmail.com>
2015-04-07 14:37:05 +0200

    config imageupload for openshift, add cleaner, edit gitignore for uploads

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,7 +1,6 @@
 # encoding: utf-8
 
 class AvatarUploader < CarrierWave::Uploader::Base
-
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
@@ -10,10 +9,20 @@ class AvatarUploader < CarrierWave::Uploader::Base
   storage :file
   # storage :fog
 
+  # hooks
+  after :remove, :delete_empty_upload_dirs
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    prefix = ENV['OPENSHIFT_DATA_DIR'] ? "#{ENV['OPENSHIFT_DATA_DIR']}/" : ""
+    "#{prefix}uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # override url to find images in production
+  def url(*args)
+    return "/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}/#{File.basename(file.path)}" if ENV['OPENSHIFT_DATA_DIR'] && file
+    super
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -44,5 +53,14 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+
+  private
+
+    def delete_empty_upload_dirs
+      path = ::File.expand_path(store_dir, root)
+      Dir.delete(path) # fails if path not empty dir
+    rescue SystemCallError
+      true # nothing, the dir is not empty
+    end
 
 end
