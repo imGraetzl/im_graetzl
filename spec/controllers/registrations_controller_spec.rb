e7401b8785f288b6334c9c3529f4c74fae8c0fb1
Max Mulatz <klappradla@gmail.com>
2015-04-14 23:22:12 +0200

    registration controller specs

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -2,18 +2,38 @@ require 'rails_helper'
 
 RSpec.describe RegistrationsController, type: :controller do
 
-  before(:each) do
-    @request.env["devise.mapping"] = Devise.mappings[:user]
-    @graetzl = create(:graetzl)
+  before { @request.env["devise.mapping"] = Devise.mappings[:user] }
+
+  describe 'GET new' do
+
+    context 'with no graetzl id in session' do
+      it 'redirects to address registration' do
+        get :new
+        expect(response).to redirect_to addresses_registration_path
+      end
+    end
+
+    context 'with graetzl id but no address in session' do
+      let(:naschmarkt) { create(:naschmarkt) }
+
+      it 'renders new' do
+        session[:graetzl] = naschmarkt.id
+        get :new
+        expect(response).to render_template(:new)
+      end
+    end
   end
 
-  describe 'POST user_registration' do
+  describe 'POST create' do
     let(:attrs) { attributes_for(:user) }
+    let(:graetzl) { create(:graetzl) }
 
     context 'with address and graetzl' do
       before do
         attrs[:address_attributes] = attributes_for(:address)
-        attrs[:graetzl_attributes] = { name: @graetzl.name }
+        attrs[:graetzl_attributes] = { name: graetzl.name }
+        session[:address] = attrs[:address_attributes]
+        session[:graetzl] = graetzl.id
       end
 
       it 'creates new user' do
@@ -21,17 +41,24 @@ RSpec.describe RegistrationsController, type: :controller do
           post :create, user: attrs
         }.to change(User, :count).by(1)
       end
+
       it 'does not create graetzl' do
         expect {
           post :create, user: attrs
         }.not_to change(Graetzl, :count)
-        expect(User.last.graetzl).to eq(@graetzl)
+        expect(User.last.graetzl).to eq(graetzl)
+      end
+
+      it 'clears sessiond data' do
+        post :create, user: attrs
+        expect(session[:address]).not_to be_present
+        expect(session[:graetzl]).not_to be_present
       end
     end
 
     context 'with only graetzl' do
       before do
-        attrs[:graetzl_attributes] = { name: @graetzl.name }
+        attrs[:graetzl_attributes] = { name: graetzl.name }
       end
 
       it 'creates new user with empty address' do
@@ -49,7 +76,6 @@ RSpec.describe RegistrationsController, type: :controller do
         }.not_to change(User, :count)
       end
     end
-
   end
 
 end
