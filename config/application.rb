c687b247ed7635f9c7cfcaded95df6e27e54bf0e
Max Mulatz <klappradla@gmail.com>
2015-04-16 12:13:58 +0200

    add :de internationalization devise, defaults and user

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,11 @@ module ImGraetzl
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    #config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+
+    # Set default locale to german
+    config.i18n.default_locale = :de
+    # Set path for nested translation files
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   end
 end
