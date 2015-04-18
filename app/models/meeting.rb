c122c43da28d72a925dbd76abc8123f89132a4d4
Max Mulatz <klappradla@gmail.com>
2015-04-18 21:10:31 +0200

    add validations for meeting (+ tests)

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -8,4 +8,27 @@ class Meeting < ActiveRecord::Base
   belongs_to :user_initialized, class_name: 'User'
   
   has_and_belongs_to_many :users_going, class_name: 'User', join_table: 'meetings_users_going'
+
+  # validations
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :user_initialized, presence: true
+  validates :start, presence: true
+  validate :start_date_cannot_be_in_the_past
+  validate :graetzls_must_be_present
+
+  private
+
+    def start_date_cannot_be_in_the_past
+      if start.present? && start < Date.today
+        errors.add(:start, "can't be in the past")
+      end
+    end
+
+    def graetzls_must_be_present
+      if graetzls.size < 1
+        errors.add(:graetzls, 'must at least be one')
+      end
+    end
+
 end
--- spec/models/meeting_spec.rb
+++ spec/models/meeting_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Meeting, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  # check factory
+  it 'has a valid factory' do
+    expect(build_stubbed(:meeting)).to be_valid
+  end
+
+  # validations
+  describe 'validations' do
+    it 'invalid without name' do
+      expect(build(:meeting, name: nil)).not_to be_valid
+    end
+
+    it 'invalid without description' do
+      expect(build(:meeting, description: nil)).not_to be_valid
+    end
+
+    it 'invalid without user_initialized' do
+      expect(build(:meeting, user_initialized: nil)).not_to be_valid
+    end
+
+    it 'invalid without start date' do
+      expect(build(:meeting, start: nil)).not_to be_valid
+    end
+
+    it 'invalid with start date in past' do
+      expect(build(:meeting, start: 1.day.ago)).not_to be_valid
+    end
+
+    it 'not invalid without address' do
+      expect(build(:meeting, address: nil)).to be_valid
+    end
+
+    it 'invalid without graetzl' do
+      expect(build(:meeting, graetzls: Array.new)).not_to be_valid
+    end
+  end
 end
