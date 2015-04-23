4a86e8f3498b6be1991fc90fdca7842f9cea9b37
ptilg <peter.tilg@gmail.com>
2015-04-23 14:59:10 +0200

    geo feature obj is now passed in hidden field as string [ci skip]

--- app/assets/javascripts/controllers/addresses.js
+++ app/assets/javascripts/controllers/addresses.js
@@ -1,14 +1,18 @@
 APP.controllers.addresses = (function() {
 
     function init() {
-
         addressSearchAutocomplete();
-
     }
 
 // ---------------------------------------------------------------------- Custom
 
     function addressSearchAutocomplete() {
+        
+        var $featureHiddenField = $("#feature");
+
+        function updateFeatureHiddenField(obj) {
+            $featureHiddenField.val(JSON.stringify(obj));
+        }
 
         var addressSearch = new Bloodhound({
             datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
@@ -17,33 +21,26 @@ APP.controllers.addresses = (function() {
             remote: {
                 url:  APP.config.adressSearchOpenGov + '%QUERY',
                 filter: function(addresses) {
-                    console.log(addresses.features);
-                    // Map the remote source JSON array to a JavaScript object array
-                    return $.map(addresses.features, function (address) {
-                        return {
-                            value: {
-                                street: address.properties.Adresse,
-                                district: address.properties.Bezirk
-                            }
-                        };
-                    });
+                    updateFeatureHiddenField(addresses.features[0]);
+                    return addresses.features;
                 }
             }
         });
-
         addressSearch.initialize();
 
         $('#address').typeahead(null, {
             name: 'addresse',
             source: addressSearch.ttAdapter(),
             displayKey: function (data) {
-                return data.value.street;
+                return data.properties.Adresse;
             },
             templates: {
                 suggestion: function(data) {
-                    return data.value.street +'<span class="district">' + data.value.district + '</span>'
+                    return data.properties.Adresse +'<span class="district">' + data.properties.Bezirk + '</span>'
                 }
             }
+        }).on('typeahead:selected typeahead:autocompleted', function(event, suggestion) {
+            updateFeatureHiddenField(suggestion);
         });
 
     }
