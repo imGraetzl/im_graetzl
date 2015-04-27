6720fe72cfad48700065ff7266fb4fd6e2a8b799
Max Mulatz <klappradla@gmail.com>
2015-04-28 00:28:28 +0200

    add named routing specs for addresses

--- spec/routing/addresses_routing_spec.rb
+++ spec/routing/addresses_routing_spec.rb
@@ -15,4 +15,19 @@ RSpec.describe AddressesController, type: :routing do
       expect(post: 'addresses/match').to route_to('addresses#match')
     end
   end
+
+  describe "named routing" do
+
+    it 'routes GET addresses/registration to #registration' do
+      expect(get: addresses_registration_path).to route_to('addresses#registration')
+    end
+
+    it 'routes POST addresses/search to #search' do
+      expect(post: addresses_search_path).to route_to('addresses#search')
+    end
+
+    it 'routes POST addresses/match to #match' do
+      expect(post: addresses_match_path).to route_to('addresses#match')
+    end
+  end
 end
