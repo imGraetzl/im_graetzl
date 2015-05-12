71c8f60d6486ec5a788c1a013d9df5363f887784
Max Mulatz <klappradla@gmail.com>
2015-05-12 16:15:56 +0200

    cleanup meeting show

--- app/helpers/meetings_helper.rb
+++ app/helpers/meetings_helper.rb
@@ -7,5 +7,9 @@ module MeetingsHelper
       "#{meeting.address.street_name} #{meeting.address.street_number}"
     end
   end
+
+  def current_going_to
+    @meeting.going_tos.find_by_user_id(current_user)
+  end
   
 end
