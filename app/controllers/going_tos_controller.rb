8d0bbc608420a57ac1678a6dfb606c10d0f96446
Max Mulatz <klappradla@gmail.com>
2015-05-13 14:01:27 +0200

    GoingTos controller answer to json request

--- app/controllers/going_tos_controller.rb
+++ app/controllers/going_tos_controller.rb
@@ -4,11 +4,21 @@ class GoingTosController < ApplicationController
   def create
     @meeting = Meeting.find(params[:meeting_id])
     current_user.go_to(@meeting)
+
+    respond_to do |format|
+      format.js
+      format.json { render json: @meeting }
+    end
   end
 
   def destroy
     going_to = GoingTo.find(params[:id])
     @meeting = going_to.meeting
     going_to.destroy
+    
+    respond_to do |format|
+      format.js
+      format.json { render json: @meeting }
+    end
   end
 end
