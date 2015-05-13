b105e50ad27d6e9e7448ca87a2b92c25ac3dc3e0
Max Mulatz <klappradla@gmail.com>
2015-05-13 12:47:28 +0200

    disable meeting date validation

--- spec/models/meeting_spec.rb
+++ spec/models/meeting_spec.rb
@@ -17,17 +17,17 @@ RSpec.describe Meeting, type: :model do
       expect(build(:meeting_full, description: nil)).not_to be_valid
     end
 
-    it 'invalid with starts_at in past' do
-      expect(build(:meeting_full, starts_at: 1.day.ago)).not_to be_valid
-    end
+    # it 'invalid with starts_at in past' do
+    #   expect(build(:meeting_full, starts_at: 1.day.ago)).not_to be_valid
+    # end
 
-    it 'invalid with ends_at in past' do
-      expect(build(:meeting_full, ends_at: 1.day.ago)).not_to be_valid
-    end
+    # it 'invalid with ends_at in past' do
+    #   expect(build(:meeting_full, ends_at: 1.day.ago)).not_to be_valid
+    # end
 
-    it 'invalid with ends_at before_starts_at' do
-      expect(build(:meeting_full, starts_at: Time.now + 1.day, ends_at: Time.now)).not_to be_valid
-    end
+    # it 'invalid with ends_at before_starts_at' do
+    #   expect(build(:meeting_full, starts_at: Time.now + 1.day, ends_at: Time.now)).not_to be_valid
+    # end
   end
 
   # instance methods
