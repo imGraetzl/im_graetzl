7e236641b13f20a95c54d55875114b0a90cb426a
Max Mulatz <klappradla@gmail.com>
2015-05-15 11:33:45 +0200

    remove unused stuff in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,11 +38,6 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-  # seed db before test suite
-  config.before(:suite) do
-    #load "#{Rails.root}/db/seeds.rb"
-  end
-
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
