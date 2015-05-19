98dc2a07d89a3ef8f534337b441793b6e5f603bc
Max Mulatz <klappradla@gmail.com>
2015-05-19 12:52:34 +0200

    change address_value helper to use address as param

--- app/helpers/meetings_helper.rb
+++ app/helpers/meetings_helper.rb
@@ -1,10 +1,10 @@
 module MeetingsHelper
 
-  def address_value(meeting)
-    if meeting.address.street_name.blank?
+  def address_value(address)
+    if address.street_name.blank?
       nil
     else
-      "#{meeting.address.street_name} #{meeting.address.street_number}"
+      "#{address.street_name} #{address.street_number}"
     end
   end
 
