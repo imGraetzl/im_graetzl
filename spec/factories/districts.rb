ff6745e4c324940a4ce71b9f3c5ac0089c13be9d
Max Mulatz <klappradla@gmail.com>
2015-05-22 00:39:40 +0200

    add factory for district

--- spec/factories/districts.rb
+++ spec/factories/districts.rb
@@ -1,8 +1,11 @@
 FactoryGirl.define do
   factory :district do
-    name "MyString"
-zip "MyString"
-area ""
+    name { Faker::Address.city }
+    zip { Faker::Address.zip }
+    area 'POLYGON ((100.0 0.0, 101.0 0.0, 101.0 1.0, 100.0 1.0, 100.0 0.0))'
   end
 
 end
+
+
+
--- spec/models/district_spec.rb
+++ spec/models/district_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe District, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:district) { build(:district) }
+
+  describe '#long_name' do
+    let(:named_district) { build(:district, name: 'district', zip: '1234') }
+
+    it 'concats name and zip' do
+      expect(named_district.long_name).to eq('district-1234')
+    end
+  end
 end
