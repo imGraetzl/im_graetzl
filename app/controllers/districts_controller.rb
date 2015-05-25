5914c548b0acd800ff6964a16f04998f340c92b1
Max Mulatz <klappradla@gmail.com>
2015-05-25 18:01:03 +0200

    add 4 meetings to graetzl show and index

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -1,17 +1,11 @@
 class DistrictsController < ApplicationController
   def index
     @districts = District.all
-    @meetings = Meeting.all
+    @meetings = Meeting.limit(4)
   end
 
   def show
     @district = District.find(params[:id])
-    @meetings = @district.graetzls.collect(&:meetings).flatten.uniq
-    #@meetings_with_graetzl = @district.graetzls.collect{ |g| [g.id, g.meetings.flatten] }
+    @meetings = @district.graetzls.collect(&:meetings).flatten.uniq.take(4)
   end
 end
-
-
-
-#Meeting.all.collect{|g| g.graetzls.pluck(:id)}.uniq
-#District.find_by_zip('1150').graetzls.collect{|g| g.meetings.pluck(:id)}.uniq
