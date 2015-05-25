0a266337b389c3d0301373480270ad038f1f77ae
Max Mulatz <klappradla@gmail.com>
2015-05-25 17:54:00 +0200

    show meetings on graetzl page

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -1,9 +1,17 @@
 class DistrictsController < ApplicationController
   def index
     @districts = District.all
+    @meetings = Meeting.all
   end
 
   def show
     @district = District.find(params[:id])
+    @meetings = @district.graetzls.collect(&:meetings).flatten.uniq
+    #@meetings_with_graetzl = @district.graetzls.collect{ |g| [g.id, g.meetings.flatten] }
   end
 end
+
+
+
+#Meeting.all.collect{|g| g.graetzls.pluck(:id)}.uniq
+#District.find_by_zip('1150').graetzls.collect{|g| g.meetings.pluck(:id)}.uniq
