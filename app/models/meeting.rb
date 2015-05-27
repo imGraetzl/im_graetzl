e1e58836e6e22ed82c89ecf75d2942a9253868cb
Max Mulatz <klappradla@gmail.com>
2015-05-27 14:04:18 +0200

    forgot to delete comment

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -8,7 +8,6 @@ class Meeting < ActiveRecord::Base
   # associations
   has_many :graetzl_meetings
   has_many :graetzls, through: :graetzl_meetings
-  #has_and_belongs_to_many :graetzls
 
   has_one :address, as: :addressable, dependent: :destroy
   accepts_nested_attributes_for :address
