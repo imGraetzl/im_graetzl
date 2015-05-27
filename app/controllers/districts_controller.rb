77bf90857e4681d375da2503d28b48eaa012d0ac
Max Mulatz <klappradla@gmail.com>
2015-05-27 17:09:19 +0200

    show only upcoming or nil dated meetings on district pages

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -1,11 +1,16 @@
 class DistrictsController < ApplicationController
   def index
     @districts = District.all
-    @meetings = Meeting.limit(4)
+    upcoming = Meeting.where(starts_at_date: nil).concat(Meeting.where('starts_at_date > ?', Date.yesterday)).map(&:id)
+    @meetings = Meeting.where(id: upcoming).take(4)
   end
 
   def show
     @district = District.find(params[:id])
-    @meetings = @district.graetzls.collect(&:meetings).flatten.uniq.take(4)
+    upcoming = Meeting.where(starts_at_date: nil).concat(Meeting.where('starts_at_date > ?', Date.yesterday)).map(&:id)
+    @meetings = Meeting.where(id: upcoming)
+                        .joins(:graetzl_meetings)
+                        .where(graetzl_meetings: { graetzl: @district.graetzls })
+                        .take(4)
   end
 end
