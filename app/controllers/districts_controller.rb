50b186d8a36161871644ae0ea6276a29b6af5f6e
Max Mulatz <klappradla@gmail.com>
2015-05-28 17:17:05 +0200

    update district and meeting partials to fit new data model

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -9,8 +9,7 @@ class DistrictsController < ApplicationController
     @district = District.find(params[:id])
     upcoming = Meeting.where(starts_at_date: nil).concat(Meeting.where('starts_at_date > ?', Date.yesterday)).map(&:id)
     @meetings = Meeting.where(id: upcoming)
-                        .joins(:graetzl_meetings)
-                        .where(graetzl_meetings: { graetzl: @district.graetzls })
+                        .where(graetzl: @district.graetzls)
                         .take(4)
   end
 end
