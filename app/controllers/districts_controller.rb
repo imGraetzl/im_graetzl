b6afa36f9ed39ce0b2046402f1b26cd9e37334f2
Max Mulatz <klappradla@gmail.com>
2015-05-29 01:37:46 +0200

    change queries for districts

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -1,15 +1,12 @@
 class DistrictsController < ApplicationController
   def index
-    @districts = District.all
-    upcoming = Meeting.where(starts_at_date: nil).concat(Meeting.where('starts_at_date > ?', Date.yesterday)).map(&:id)
-    @meetings = Meeting.where(id: upcoming).take(4)
+    @districts = District.all()
+    @meetings = Meeting.where(starts_at_date: [nil, Date.yesterday..Date.yesterday.next_year]).limit(4)
   end
 
   def show
     @district = District.find(params[:id])
-    upcoming = Meeting.where(starts_at_date: nil).concat(Meeting.where('starts_at_date > ?', Date.yesterday)).map(&:id)
-    @meetings = Meeting.where(id: upcoming)
-                        .where(graetzl: @district.graetzls)
-                        .take(4)
+    @meetings = Meeting.where(graetzl: @district.graetzls,
+                starts_at_date: [nil, Date.yesterday..Date.yesterday.next_year]).limit(4)
   end
 end
