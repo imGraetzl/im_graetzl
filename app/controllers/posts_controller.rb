e70f19f3de534ec528f3f33f3475573940944af8
Max Mulatz <klappradla@gmail.com>
2015-06-01 21:25:33 +0200

    add posts_controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+class PostsController < ApplicationController
+  before_filter :authenticate_user!
+  before_filter :set_graetzl
+
+  def create
+    @post = @graetzl.posts.build(post_params)
+    if @post.save
+      redirect_to @graetzl
+    else
+      render nothing: true
+    end
+  end
+
+  private
+
+    def set_graetzl
+      @graetzl = Graetzl.find(params[:graetzl_id])
+    end
+
+    def post_params
+      params.require(:post).permit(:content).merge(user_id: current_user.id)
+    end
+end
