b05a0a2d913d165861d37e446e8537ce4e5ea92f
ptilg <peter.tilg@gmail.com>
2015-06-02 14:19:08 +0200

    stream building blocks

--- app/assets/javascripts/utils/generic.js
+++ app/assets/javascripts/utils/generic.js
@@ -26,6 +26,36 @@ APP.utils = (function() {
 
 
 //micro jQuery plugin to check if a given element exists
-jQuery.fn.exists = function(){
+$.fn.exists = function(){
     return this.length;
+};
+
+
+$.fn.showMoreOnTyping = function( options ) {
+
+    var settings = $.extend({
+        charCount: 5,
+        elementsToShow: function() {}
+    }, options );
+
+    return this.each(function() {
+
+        var $this = $(this);
+        var $elementsToShow = settings.elementsToShow.call(this);
+
+        if($this.val().length > settings.charCount) {
+            $elementsToShow.show();
+        }
+
+        $this.on("keyup", function () {
+
+            if ($this.val().length > settings.charCount) {
+                $elementsToShow.fadeIn();
+            } else {
+                $elementsToShow.hide();
+            }
+        });
+
+    });
+
 };
