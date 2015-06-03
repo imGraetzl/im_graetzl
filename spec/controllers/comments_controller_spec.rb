62562d36a23ef95bb006c35b89f7af572640ffb4
Max Mulatz <klappradla@gmail.com>
2015-06-03 15:29:57 +0200

    start comment controller specs

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
+  let(:user) { create(:user) }
 
+  describe 'POST create' do
+    let(:params) {
+      {
+        comment: { content: 'content_text' }
+      }
+    }
+
+    context 'when no current_user' do
+      it 'redirects to login_page' do
+        xhr :post, :create, params
+        expect(response).to render_template(session[:new])
+      end
+    end
+
+    context 'when current_user' do
+      before { sign_in user }
+    end
+  end
 end
