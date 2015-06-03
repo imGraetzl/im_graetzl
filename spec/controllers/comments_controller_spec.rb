e0b443a81af82d593835f9df7a2ad0d35993d2f9
Max Mulatz <klappradla@gmail.com>
2015-06-03 15:49:42 +0200

    more comment controller specs

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -2,11 +2,16 @@ require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
   let(:user) { create(:user) }
+  let(:commentable_post) { create(:post) }
+  let(:form_id_hex) { SecureRandom.hex(4) }
 
   describe 'POST create' do
     let(:params) {
       {
-        comment: { content: 'content_text' }
+        comment: { content: 'content_text' },
+        commentable_type: commentable_post.class,
+        commentable_id: commentable_post.id,
+        form_id: form_id_hex
       }
     }
 
@@ -19,6 +24,16 @@ RSpec.describe CommentsController, type: :controller do
 
     context 'when current_user' do
       before { sign_in user }
+
+      it 'assigns @commentable' do
+        xhr :post, :create, params
+        expect(assigns(:commentable)).to eq(commentable_post)
+      end
+
+      it 'assigns @form_id' do
+        xhr :post, :create, params
+        expect(assigns(:form_id)).to eq(form_id_hex)
+      end
     end
   end
 end
