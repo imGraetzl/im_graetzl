c10f311c51bf59ad75d1a8bdfa792d0baf13df16
Max Mulatz <klappradla@gmail.com>
2015-06-09 16:33:22 +0200

    improve sort_by query part

--- app/models/graetzl.rb
+++ app/models/graetzl.rb
@@ -21,9 +21,6 @@ class Graetzl < ActiveRecord::Base
           a[:trackable_id].in(meetings.pluck(:id)).and(a[:trackable_type].eq('Meeting'))).or(
           a[:trackable_id].in(posts.pluck(:id)).and(a[:trackable_type].eq('Post'))))
       .order(:trackable_id, :trackable_type, created_at: :desc)
-      .sort_by{|activity| activity.created_at}.reverse
+      .sort_by(&:created_at).reverse
   end
 end
-
-#.select('DISTINCT ON(trackable_id, trackable_type) *')
-#.select(Arel::Nodes::DistinctOn(a[:trackable_id]))
