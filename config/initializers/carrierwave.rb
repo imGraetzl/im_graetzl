9b359b98d818d1a781e867149b583edae91e11ef
Max Mulatz <klappradla@gmail.com>
2015-06-12 13:24:54 +0200

    leave (openshift-specific) carrierwave config out of initializers/carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,8 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
     config.enable_processing = true
-    config.root = ENV['OPENSHIFT_DATA_DIR']
+    #config.root = ENV['OPENSHIFT_DATA_DIR']
+    config.root = File.join(Rails.root, 'public/')
     config.cache_dir = config.root + 'uploads'
   elsif Rails.env.test?
     config.root = File.join(Rails.root, 'public/')
