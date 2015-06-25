deab93d5bc3658c98297abdc363e5abea26ffc3f
Max Mulatz <klappradla@gmail.com>
2015-06-25 17:54:10 +0200

    dry up stram post activity

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,19 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { "#{page_title} | "}    
   end
+
+  def activity_description(user, activity)
+    puts activity.key
+    case activity.key
+    when 'post.create'
+      "Neuer post von <a href=''>#{user.username if user}</a>".html_safe
+    when 'post.comment'
+      "Post von <a href=''>#{user.username if user}</a>".html_safe
+    when 'meeting.create'
+    when 'meeting.comment'
+    when 'meeting.go_to'
+    else
+    end
+    
+  end
 end
