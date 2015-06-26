6de5b92ce908446381abb16d1c2b098b79b9e4a9
Max Mulatz <klappradla@gmail.com>
2015-06-26 14:48:21 +0200

    refactor stream

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,14 +3,16 @@ module ApplicationHelper
     content_for(:title) { "#{page_title} | "}    
   end
 
-  def entry_description(user, entry)
-    case entry.class.name.demodulize.downcase
+  def activity_description(user, activity)
+    case activity.class.name.demodulize.downcase
     when 'activity'
-      case entry.key
+      case activity.key
       when 'post.create'
         "Neuer Post von <a href=''>#{user.username if user}</a>".html_safe
       when 'meeting.create'
         "<a href=''>#{user.username if user}</a> hat ein Treffen erstellt".html_safe
+      when 'meeting.go_to'
+        "Treffen von <a href=''>#{user.username if user}</a> hat neue Teilnehmer".html_safe
       else
       end
     when 'post'
