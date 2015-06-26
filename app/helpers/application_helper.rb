06559b4ac4c44f99dc8713d2ee78b48129bb3657
Max Mulatz <klappradla@gmail.com>
2015-06-26 16:06:50 +0200

    rudmentary stream for meeting#show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,7 +20,7 @@ module ApplicationHelper
     when 'meeting'
       "Treffen von <a href=''>#{user.username if user}</a>".html_safe
     when 'comment'
-      "Kommentar von <a href=''>#{user.username if user}</a>".html_safe
+      "<a href=''>#{user.username if user}</a>".html_safe
     else
     end    
   end
