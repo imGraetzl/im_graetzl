62cda01d839bfc2baa94ebe67b4b36983e8605de
Max Mulatz <klappradla@gmail.com>
2015-06-30 15:01:38 +0200

    update district model specs

--- spec/models/district_spec.rb
+++ spec/models/district_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe District, type: :model do
-  let(:district) { build(:district) }
 
   describe 'default scope' do
     let(:third_district) { create(:district, zip: '1030') }
@@ -11,6 +10,41 @@ RSpec.describe District, type: :model do
     it 'retrieves ordered by zip' do
       expect(District.all).to eq [first_district, second_district, third_district]
     end
+  end
+
+  describe '#graetzls' do
+    let(:district) { create(:district) }
+
+    let!(:g_covering) { create(:graetzl) }
+    let!(:g_within) { create(:graetzl,
+      area: 'POLYGON ((1.0 1.0, 1.0 2.0, 2.0 2.0, 2.0 1.0, 1.0 1.0))') }
+    let!(:g_overlapping) { create(:graetzl,
+      area: 'POLYGON ((1.0 1.0, 1.0 15.0, 15.0 15.0, 15.0 1.0, 1.0 1.0))') }
+    let!(:g_outside) { create(:graetzl,
+      area: 'POLYGON ((15.0 15.0, 15.0 20.0, 20.0 20.0, 15.0 15.0))') }
+
+    subject(:graetzls) { district.graetzls }
+
+    it 'returns covering/overlapping areas' do
+      expect(graetzls.size).to eq 3
+    end
+
+    it 'includes areas fully covering' do
+      expect(graetzls).to include g_covering
+    end
+
+    it 'includes areas within' do
+      expect(graetzls).to include g_within
+    end
+
+    it 'includes areas overlapping' do
+      expect(graetzls).to include g_overlapping
+    end
+
+    it 'excludes areas outside' do
+      expect(graetzls).not_to include g_outside
+    end
+
   end
 
   describe '#long_name' do
