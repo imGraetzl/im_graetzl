cfa52d3767fd590113606321f8c1761e99f5c6a5
Max Mulatz <klappradla@gmail.com>
2015-07-03 14:57:59 +0200

    add meeting boxes to home_out page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,8 @@ class StaticPagesController < ApplicationController
   def home
     if user_signed_in?
       redirect_to current_user.graetzl
-    end    
+    end
+    @meetings = Meeting.last(2)
   end
 
   def meetingCreate
