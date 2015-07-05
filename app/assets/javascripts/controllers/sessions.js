c64d79663af459e6a2e66885dfd25469aa31ff3c
ptilg <peter.tilg@gmail.com>
2015-07-05 14:03:04 +0200

    refactor forms (register, login, address)

---
+++ app/assets/javascripts/controllers/sessions.js
@@ -0,0 +1,17 @@
+APP.controllers.sessions = (function() {
+
+    function init() {
+
+        console.log("dreck");
+        APP.components.inputTextareaMovingLabel();
+
+    }
+
+
+// ---------------------------------------------------------------------- Public
+
+    return {
+        init: init
+    }
+
+})();
