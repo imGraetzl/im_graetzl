132eb1d73e1676670bea1169146ffbd8976c788f
Max Mulatz <klappradla@gmail.com>
2015-07-07 17:53:22 +0200

    restict activities shown in stream

--- app/models/graetzl.rb
+++ app/models/graetzl.rb
@@ -7,6 +7,12 @@ class Graetzl < ActiveRecord::Base
   has_many :meetings, dependent: :destroy
   has_many :posts, dependent: :destroy
 
+  # attributes
+  STREAM_ACTIVITY_KEYS = [
+    'meeting.comment', 'meeting.create', 'meeting.go_to',
+    'post.comment', 'post.create'
+  ]
+
   # instance methods
   def districts
     District.where('ST_INTERSECTS(area, :graetzl)', graetzl: self.area)
@@ -16,6 +22,7 @@ class Graetzl < ActiveRecord::Base
     a = PublicActivity::Activity.arel_table
     PublicActivity::Activity
       .select('DISTINCT ON(trackable_id, trackable_type) *')
+      .where(a[:key].in(STREAM_ACTIVITY_KEYS))
       .where(
         a[:owner_id].in(users.pluck(:id)).or(
           a[:trackable_id].in(meetings.pluck(:id)).and(a[:trackable_type].eq('Meeting'))).or(
