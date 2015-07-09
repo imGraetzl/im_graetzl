77ce5001ceaab22885e1c49b5923287e254e277e
Tawan Sierek <tawan.sierek@gmail.com>
2015-07-09 12:18:01 +0200

    Stabilize tests - generate unique usernames
    Some tests are failing because Faker::Internet.user_name
    doesn't have enough entropy in order to generate only unique
    usernames for within a full test runs which leads to failing
    tests since a user object cannot be created.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ require 'faker'
 
 FactoryGirl.define do
   factory :user do
-    username { Faker::Internet.user_name }
+    sequence(:username) { |n| "user_#{n}" }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
