d483e2e34d292aa25245d71bbcc73d4bb78f1fce
Max Mulatz <klappradla@gmail.com>
2015-07-12 17:18:39 +0200

    add tests for address form fields (view spec)

---
+++ spec/views/devise/registrations/address.html.erb_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'devise/registrations/address', type: :view do
+
+  describe 'address form' do
+    before { render }
+
+    it 'has :address field' do
+      expect(rendered).to have_field(:address)
+    end
+
+    it 'has hidden :feature field' do
+      expect(rendered).to have_selector('#feature', visible: false)
+    end
+  end
+end
