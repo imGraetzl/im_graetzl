729da2f6411ff871356dc815d7c1f8a5860450ae
Max Mulatz <klappradla@gmail.com>
2015-07-12 20:15:28 +0200

    update feature specs to use context

--- spec/features/registrations_spec.rb
+++ spec/features/registrations_spec.rb
@@ -4,70 +4,83 @@ RSpec.feature 'Registration', type: :feature do
 
   before { visit user_registration_address_path }
 
-  scenario 'address matches only one graetzl', js: true do
-    create(:naschmarkt)
-    address = build(:esterhazygasse)
+  context 'address matches single graetzl' do
+    let!(:graetzl) { create(:naschmarkt) }
+    let(:address) { build(:esterhazygasse) }
 
-    fill_in :address, with: "#{address.street_name} #{address.street_number}"
-    sleep 2
-    click_button 'Weiter'
+    scenario 'enter valid userdata', js: true do
+      fill_in :address, with: "#{address.street_name} #{address.street_number}"
+      sleep 2
+      click_button 'Weiter'
 
-    expect(page).to have_text('Willkommen im Grätzl Naschmarkt')
-    expect(page).to have_link('Nicht dein Grätzl?', href: user_registration_graetzl_path)
+      expect(page).to have_text("Willkommen im Grätzl #{graetzl.name}")
+      expect(page).to have_link('Nicht dein Grätzl?', href: user_registration_graetzl_path)
 
-    fill_in_user_data
-    click_button 'Jetzt registrieren'
+      fill_in_user_data
+      click_button 'Jetzt registrieren'
+    end
   end
 
-  scenario 'address matches multiple graetzl', js: true do
-    seestadt_aspern = create(:seestadt_aspern)
-    create(:aspern)
-    address = build(:seestadt)
+  context 'address matches multiple graetzls' do
+    let!(:seestadt_aspern) { create(:seestadt_aspern) }
+    let!(:aspern) { create(:aspern) }
+    let(:address) { build(:seestadt) }
 
-    fill_in :address, with: "#{address.street_name}"
-    sleep 2
-    click_button 'Weiter'
+    scenario 'enter valid userdata', js: true do
+      fill_in :address, with: "#{address.street_name}"
+      sleep 2
+      click_button 'Weiter'
 
-    expect(page).to have_text("Unter #{address.street_name} konnten wir 2 Grätzl finden.")
-    expect(page).to have_field('graetzl', type: 'radio', count: 2, visible: false)
+      expect(page).to have_text("Unter #{address.street_name} konnten wir 2 Grätzl finden.")
+      expect(page).to have_field('graetzl', type: 'radio', count: 2, visible: false)
 
-    find("label[for=graetzl_#{seestadt_aspern.id}]").click
-    click_button 'Weiter'
+      find("label[for=graetzl_#{seestadt_aspern.id}]").click
+      click_button 'Weiter'
 
-    expect(page).to have_text('Willkommen im Grätzl Seestadt Aspern')
-    expect(page).to have_link('Nicht dein Grätzl?', href: user_registration_graetzl_path)
+      expect(page).to have_text("Willkommen im Grätzl #{seestadt_aspern.name}")
+      expect(page).to have_link('Nicht dein Grätzl?', href: user_registration_graetzl_path)
 
-    fill_in_user_data
-    click_button 'Jetzt registrieren'
+      fill_in_user_data
+      click_button 'Jetzt registrieren'
+    end
   end
 
-  scenario 'address matches no graetzl', js: true do
-    district_1 = create(:district)
-    district_2 = create(:district,
+  context 'address matches no graetzl' do
+    let!(:district_1) { create(:district) }
+    let!(:district_2) { create(:district,
       area: 'POLYGON ((20.0 20.0, 20.0 30.0, 30.0 30.0, 30.0 20.0, 20.0 20.0))')
-    graetzl_1 = create(:graetzl)
-    graetzl_2 = create(:graetzl,
+    }
+    let!(:graetzl_1) { create(:graetzl) }
+    let!(:graetzl_2) { create(:graetzl,
       area: 'POLYGON ((25.0 25.0, 25.0 26.0, 27.0 27.0, 25.0 25.0))')
+    }
 
-    fill_in :address, with: 'qwertzuiopü'
-    sleep 2
-    click_button 'Weiter'
+    scenario 'enter valid userdata', js: true do
+      fill_in :address, with: 'qwertzuiopü'
+      sleep 2
+      click_button 'Weiter'
 
-    expect(page).to have_text('Unter qwertzuiopü konnten wir leider kein Grätzl finden.')
+      expect(page).to have_text('Unter qwertzuiopü konnten wir leider kein Grätzl finden.')
 
-    select "#{district_2.zip}", from: :district_id
-    sleep 2
-    select "#{graetzl_2.name}", from: :graetzl
-    click_button 'Weiter'
+      select "#{district_2.zip}", from: :district_id
+      sleep 2
+      select "#{graetzl_2.name}", from: :graetzl
+      click_button 'Weiter'
 
-    expect(page).to have_text("Willkommen im Grätzl #{graetzl_2.name}")
-    expect(page).to have_link('Nicht dein Grätzl?', href: user_registration_graetzl_path)  
+      expect(page).to have_text("Willkommen im Grätzl #{graetzl_2.name}")
+      expect(page).to have_link('Nicht dein Grätzl?', href: user_registration_graetzl_path)  
 
-    fill_in_user_data
-    click_button 'Jetzt registrieren' 
+      fill_in_user_data
+      click_button 'Jetzt registrieren' 
+    end
   end
 
   private
+
+    def fill_in_address(address)
+      fill_in :address, with: "#{address.street_name} #{address.street_number}"
+      sleep 2
+    end
 
     def fill_in_user_data
       fill_in :user_username, with: 'newuser'
