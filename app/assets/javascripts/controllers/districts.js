6ab25fb1734c219b925d7f26257cf745f1c7b160
ptilg <peter.tilg@gmail.com>
2015-08-03 15:15:52 +0200

    map first prototype with clickthrought

---
+++ app/assets/javascripts/controllers/districts.js
@@ -0,0 +1,42 @@
+APP.controllers.districts = (function() {
+
+    var map =  APP.components.graetzlMap;
+
+    function init() {
+
+        if($('section.vienna').exists()){
+            map.init(function() {
+                    map.showMapDistrict(null, {
+                        interactive: true
+                    });
+                }
+            );
+        }
+
+
+        if($('section.districts').exists()){
+            var mapvisible= jQuery('section.districts').data('mapvisible');
+            map.init(function() {
+                    map.showMapDistrict(mapvisible.districts, {
+                        style: $.extend(map.styles.mint, {
+                            weight: 0,
+                            fillOpacity: 0.5
+                        })
+                    });
+                    map.showMapGraetzl(null, mapvisible.districts, {
+                        interactive: true,
+                        zoomAfterRender: false
+                    });
+                }
+            )
+        }
+
+
+
+    }
+
+    return {
+        init: init
+    }
+
+})();
