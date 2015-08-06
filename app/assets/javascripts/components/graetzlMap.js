c9c9fd4b31f060d8516dbb394de516ae05802e28
ptilg <peter.tilg@gmail.com>
2015-08-06 15:38:51 +0200

    Wien, Bezirk, Grätzl Seiten -mapview Nachtrag

--- app/assets/javascripts/components/graetzlMap.js
+++ app/assets/javascripts/components/graetzlMap.js
@@ -2,8 +2,8 @@ APP.components.graetzlMap = (function() {
     var map,
         geoVienna,
         geoGraetzl,
-     mainLayer =  L.tileLayer.provider('Stamen.Watercolor'),
-    //    mainLayer = L.tileLayer.provider('MapBox', { id: 'peckomingo.lb8m2cga', accessToken: 'pk.eyJ1IjoicGVja29taW5nbyIsImEiOiJoVHNQM29zIn0.AVmpyDYApR5mryMCJB1ryw'}),
+    // mainLayer =  L.tileLayer.provider('Stamen.Watercolor'),
+        mainLayer = L.tileLayer.provider('MapBox', { id: 'peckomingo.lb8m2cga', accessToken: 'pk.eyJ1IjoicGVja29taW5nbyIsImEiOiJoVHNQM29zIn0.AVmpyDYApR5mryMCJB1ryw'}),
 
         styles = {
             mint: {
