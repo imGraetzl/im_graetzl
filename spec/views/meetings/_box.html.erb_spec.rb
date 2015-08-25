e53349cb2f744f81e14a6aa26177b5398ccd02e3
Max Mulatz <klappradla@gmail.com>
2015-08-25 15:54:54 +0200

    show location badge only if iniator owns location Close #44

---
+++ spec/views/meetings/_box.html.erb_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe 'meetings/_box', type: :view do
+  # pass locals
+  # render :partial => "widgets/widget.html.erb", :locals => {:widget => widget}
+  context 'without location' do
+    let(:meeting) { create(:meeting, location: nil) }
+    before { render 'meetings/box', meeting: meeting }
+
+    it 'displays graetzl flag' do
+      expect(rendered).to have_selector('div.sideflag', text: meeting.graetzl.name)
+    end
+  end
+
+  context 'with location' do
+    let(:meeting) { create(:meeting, location: create(:location)) }
+
+    context 'when not owned by initiator' do
+      before do
+        allow(meeting).to receive(:initiator) { create(:user) }
+        allow(meeting.location).to receive(:users) { [create(:user)] }
+        render 'meetings/box', meeting: meeting
+      end
+
+      it 'displays graetzl flag' do
+        expect(rendered).to have_selector('div.sideflag', text: meeting.graetzl.name)
+      end
+    end
+
+    context 'when owned by initiator' do
+      let(:user) { create(:user) }
+      before do
+        allow(meeting).to receive(:initiator) { user }
+        allow(meeting.location).to receive(:users) { [user] }
+        render 'meetings/box', meeting: meeting
+      end
+
+      it 'does not display graetzl flag' do
+        expect(rendered).not_to have_selector('div.sideflag')
+      end
+
+      it 'displays location badge' do
+        expect(rendered).to have_selector('div.medalBadge')
+      end
+    end
+  end
+end
