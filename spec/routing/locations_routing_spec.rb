39bea9f90c6e14f2a0c8fff8af390908df47d9b3
Max Mulatz <klappradla@gmail.com>
2015-08-26 11:52:52 +0200

    update routing specs for locations

--- spec/routing/locations_routing_spec.rb
+++ spec/routing/locations_routing_spec.rb
@@ -6,14 +6,23 @@ RSpec.describe LocationsController, type: :routing do
   
   describe 'routing ' do
 
-    it 'routes GET /locations/new to #new' do
-      expect(get: '/locations/new').to route_to(
+    it 'routes GET /graetzl-slug/locations/location-slug to #show' do
+      expect(get: '/graetzl-slug/locations/location-slug').to route_to(
         controller: 'locations',
-        action: 'new')
+        action: 'show',
+        graetzl_id: 'graetzl-slug',
+        id: 'location-slug')
     end
 
-    it 'routes POST /locations/new to #new' do
-      expect(post: '/locations/new').to route_to(
+    it 'routes GET /graetzl-slug/locations to #index' do
+      expect(get: '/graetzl-slug/locations').to route_to(
+        controller: 'locations',
+        action: 'index',
+        graetzl_id: 'graetzl-slug')
+    end
+
+    it 'routes GET /locations/new to #new' do
+      expect(get: '/locations/new').to route_to(
         controller: 'locations',
         action: 'new')
     end
@@ -24,23 +33,37 @@ RSpec.describe LocationsController, type: :routing do
         action: 'create')
     end
 
-    # it 'routes GET /locations/location_slug to #show' do
-    #   expect(get: '/graetzl_slug/locations/location_slug').to route_to(
-    #     controller: 'locations',
-    #     action: 'show',
-    #     graetzl_id: 'graetzl_slug',
-    #     id: 'location_slug')
-    # end
-
-    it 'routes GET /locations/location_slug/edit to #edit' do
-      expect(get: '/locations/location_slug/edit').to route_to(
+    it 'routes GET /locations/location-slug/edit to #edit' do
+      expect(get: '/locations/location-slug/edit').to route_to(
         controller: 'locations',
         action: 'edit',
-        id: 'location_slug')
+        id: 'location-slug')
+    end
+
+    it 'routes PUT /locations/location-slug to #update' do
+      expect(put: '/locations/location-slug').to route_to(
+        controller: 'locations',
+        action: 'update',
+        id: 'location-slug')
     end
   end
 
   describe 'named routing' do
+
+    it 'routes GET graetzl_location to #show' do
+      expect(get: graetzl_location_path(graetzl_id: 'graetzl-slug', id: 'location-slug')).to route_to(
+        controller: 'locations',
+        action: 'show',
+        graetzl_id: 'graetzl-slug',
+        id: 'location-slug')
+    end
+
+    it 'routes GET graetzl_locations to #index' do
+      expect(get: graetzl_locations_path(graetzl_id: 'graetzl-slug')).to route_to(
+        controller: 'locations',
+        action: 'index',
+        graetzl_id: 'graetzl-slug')
+    end
 
     it 'routes GET new_location to #new' do
       expect(get: new_location_path).to route_to(
@@ -55,18 +78,17 @@ RSpec.describe LocationsController, type: :routing do
     end
 
     it 'routes GET edit_location to #edit' do
-      expect(get: edit_location_path(id: 'location_slug')).to route_to(
+      expect(get: edit_location_path(id: 'location-slug')).to route_to(
         controller: 'locations',
         action: 'edit',
-        id: 'location_slug')
+        id: 'location-slug')
     end
 
-    # it 'routes GET graetzl_location to #show' do
-    #   expect(get: graetzl_location_path(graetzl_id: 'graetzl_slug', id: 'location_slug')).to route_to(
-    #     controller: 'locations',
-    #     action: 'show',
-    #     graetzl_id: 'graetzl_slug',
-    #     id: 'location_slug')
-    # end
+    it 'routes PUT location to #update' do
+      expect(put: location_path(id: 'location-slug')).to route_to(
+        controller: 'locations',
+        action: 'update',
+        id: 'location-slug')
+    end
   end
 end
