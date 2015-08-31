97765e2d7b3080aebda63830ac06d642f255a44b
Max Mulatz <klappradla@gmail.com>
2015-08-31 17:47:02 +0200

    remove images working

--- app/assets/javascripts/components/stream.js
+++ app/assets/javascripts/components/stream.js
@@ -3,92 +3,9 @@ APP.components.stream = (function() {
     function init() {
         APP.components.imgUploadPreview();
         inlineEditor();
+        createEntry();
 
         $(".entryCommentForm textarea, .entryCreate textarea").autogrow();
-
-        $(".stream").on("focusin focusout", ".entryCommentForm textarea, .entryCreate textarea", function(event){
-            var $parent = $(this).parents(".entryCommentForm, .entryCreate");
-            var buttonText = $parent.find('button').text();
-            
-            if (event.type === 'focusin') {
-                $parent.addClass("is-focused");
-
-                // temporary solution for better UX on ajax requests (only inline comments so far)
-                if ($parent.hasClass('entryCommentForm')) {                    
-                    var $button = $parent.find('button');
-                    var buttonText = $button.text();
-                    $parent
-                        .on("ajax:beforeSend", function() {
-                            $button.html('sendet...');
-                            //$button.prop('disabled', true);
-                        })
-                        .on("ajax:success", function(event, data, status, xhr) {
-                            console.log('call ajax success');
-                            console.log(data);
-                            $parent.before(data);
-                            // reset form:
-                            // console.log('hello hello');
-                            // console.log(data);
-                            // console.log(xhr.responseText);
-                        })
-                        .on("ajax:error", function(event, xhr, status, error) {
-                            $parent.replaceWith("<em>Es gab ein Problem, bitte versuch es später nochmal.</em>");
-                        })
-                        .on("ajax:complete", function(xhr, status) {
-                            $button.html(buttonText);
-                            $parent.trigger('reset');
-                            $parent.removeClass("is-focused");
-                            $parent.off("ajax:beforeSend");
-                            $parent.off("ajax:error");
-                            $parent.off("ajax:success");
-                            $parent.off("ajax:complete");
-                        });
-                } else {
-                    console.log('IN ELSE NOW');                   
-                    var $form = $parent.find('form.textEditor');
-                    console.log($form);
-                    $parent
-                        .on("ajax:beforeSend", function() {
-                            console.log('BEFORE SEND');
-                        })
-                        .on("ajax:complete", function(event, xhr) {
-                            // console.log(status.status);
-                            // console.log(status.status != 200);
-                            // console.log(status.status != '200');
-                            console.log('COMPLETE');
-                            // due to problems with jquery and parsing files...
-                            // use complete event an manually check for errors:
-                            if (xhr.status != 200 || !xhr.responseText) {
-                                alert('Es gab ein Problem, bitte versuch es später nochmal.');
-                            } else {
-                                console.log(xhr);
-                                $('div#stream-comment-form').after(xhr.responseText);
-                                $form.trigger('reset');
-                                $form.find('[name="comment[images_files][]"]').val('');
-                                $parent.removeClass("is-focused");
-                                $parent.off("ajax:beforeSend");
-                                $parent.off("ajax:error");
-                                $parent.off("ajax:success");
-                                $parent.off("ajax:complete");
-                            }
-                            // $button.html(buttonText);
-                            // $parent.trigger('reset');
-                            // $parent.removeClass("is-focused");
-                            // $parent.off("ajax:beforeSend");
-                            // $parent.off("ajax:error");
-                            // $parent.off("ajax:success");
-                            // $parent.off("ajax:complete");
-                        });
-                }
-
-
-            } else if (event.type === 'focusout') {
-                if (!$(this).val().length) {
-                    $parent.removeClass("is-focused");
-                }
-            }
-        });
-
     }
 
 
@@ -166,6 +83,70 @@ APP.components.stream = (function() {
                         alert("Dein Kommentar konnte nicht gelöscht werden. Bitte versuche es später nochmal");
                     }
                 });
+            }
+        });
+    }
+
+
+    // tmp solution for better ux with ajax comments/posts
+    function createEntry() {
+        $(".stream").on("focusin focusout", ".entryCommentForm textarea, .entryCreate textarea", function(event){
+            var $parent = $(this).parents(".entryCommentForm, .entryCreate");
+            var buttonText = $parent.find('button').text();
+
+            if (event.type === 'focusin') {
+                $parent.addClass("is-focused");
+
+                // temporary solution for better UX on ajax requests (only inline comments so far)               
+                var $button = $parent.find('button');
+                var buttonText = $button.text();
+                $parent
+                    .on("ajax:beforeSend", function() {
+                        console.log('BEFORE SEND');
+                        $button.html('sendet...');
+                    })
+                    .on("ajax:complete", function(event, xhr) {
+                        console.log('COMPLETE');
+                        if (xhr.status != 200 || !xhr.responseText) {
+                            alert('Es gab ein Problem, bitte versuch es später nochmal.');
+                        } else {
+                            injectContent(xhr.responseText);
+                            cleanup($parent.find('form.textEditor'));
+                        }
+                    });
+
+            } else if (event.type === 'focusout') {
+                if (!$(this).val().length) {
+                    $parent.removeClass("is-focused");
+                }
+            }
+
+
+            // inject new content in page
+            function injectContent(content) {
+                var inline = $parent.hasClass('entryCommentForm');
+                if (inline) {
+                    $parent.before(content)
+                } else {
+                    $('div#stream-comment-form').after(content);
+                }
+            }
+
+
+            function cleanup(form) {
+                $parent.removeClass("is-focused");
+                $parent.off("ajax:beforeSend");
+                $parent.off("ajax:complete");
+                $parent.removeClass("is-focused");
+
+                $button.html(buttonText);
+
+                if (form.length == 0) form = $parent;
+
+                form.trigger('reset');
+
+                form.find('.imgCrop').remove().end()
+                $("input[name$='[images_files][]']").val('');
             }
         });
     }
