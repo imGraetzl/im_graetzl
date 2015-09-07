88530f6c10cfc4f6a88dde929c7dc70a18d8fcae
Max Mulatz <klappradla@gmail.com>
2015-09-07 20:53:39 +0200

    fix location maps Close #72

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,5 +1,5 @@
 module ImagesHelper
-  def cover_photo_for(model, options = {})
+  def cover_photo_for(model, options={})
     options = cover_photo_defaults.merge(options)
     attachment_image_tag(model, :cover_photo,
                                 :fill, options[:fill][0], options[:fill][1],
@@ -7,7 +7,7 @@ module ImagesHelper
                                 class: options[:class])
   end
 
-  def avatar_for(model, options = {})
+  def avatar_for(model, options={})
     options = avatar_defaults.merge(options)
     attachment_image_tag(model, :avatar,
                                 :fill, options[:fill][0], options[:fill][1],
