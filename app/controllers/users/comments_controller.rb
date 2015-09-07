5325f94fcfd1b19f9b1728c2aa1d34a251181eff
Max Mulatz <klappradla@gmail.com>
2015-09-07 15:13:48 +0200

    remove comments

--- app/controllers/users/comments_controller.rb
+++ app/controllers/users/comments_controller.rb
@@ -4,13 +4,6 @@ class Users::CommentsController < CommentsController
   # override create for wall_posts
   def create
     @comment = @commentable.wall_posts.build(comment_params)
-    # if comment.save
-    #   @commentable.create_activity :comment, owner: current_user, recipient: comment if log_activity?
-    #   render partial: 'comment', locals: { comment: comment, comment_inline: true } and return if inline?
-    #   render partial: 'comment', layout: 'layouts/stream/element', locals: { comment: comment } and return      
-    # else
-    #   render nothing: true, status: :internal_server_error
-    # end
     super
   end
 
