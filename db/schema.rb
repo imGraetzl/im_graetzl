9f5fdd318afc762477d24fc8f766ca784eab1754
Tawan Sierek <tawan.sierek@gmail.com>
2015-09-10 09:04:56 +0200

    Close #66 Default settings for notification

--- db/schema.rb
+++ db/schema.rb
@@ -234,12 +234,12 @@ ActiveRecord::Schema.define(version: 20150908104611) do
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                         limit: 255, default: "",    null: false
-    t.string   "encrypted_password",            limit: 255, default: "",    null: false
+    t.string   "email",                         limit: 255, default: "",      null: false
+    t.string   "encrypted_password",            limit: 255, default: "",      null: false
     t.string   "reset_password_token",          limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                             default: 0,     null: false
+    t.integer  "sign_in_count",                             default: 0,       null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",            limit: 255
@@ -255,13 +255,13 @@ ActiveRecord::Schema.define(version: 20150908104611) do
     t.string   "last_name",                     limit: 255
     t.date     "birthday"
     t.integer  "gender"
-    t.boolean  "newsletter",                                default: false, null: false
+    t.boolean  "newsletter",                                default: false,   null: false
     t.integer  "graetzl_id"
     t.string   "avatar_id"
-    t.integer  "enabled_website_notifications",             default: 0
+    t.integer  "enabled_website_notifications",             default: 999999
     t.integer  "role"
     t.string   "avatar_content_type"
-    t.integer  "immediate_mail_notifications",              default: 0
+    t.integer  "immediate_mail_notifications",              default: 9999999
     t.integer  "daily_mail_notifications",                  default: 0
     t.integer  "weekly_mail_notifications",                 default: 0
     t.string   "slug"
