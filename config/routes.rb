d6754f38b10f949d3d240a98456cf08f598b9d18
ptilg <peter.tilg@gmail.com>
2015-09-16 11:41:24 +0200

    add route for zuckerlOverview

--- config/routes.rb
+++ config/routes.rb
@@ -51,6 +51,7 @@ Rails.application.routes.draw do
   get 'static_pages/homeOut'
   get 'static_pages/userprofile'
   get 'static_pages/location'
+  get 'static_pages/zuckerlOverview'
   root 'static_pages#home'
 
   resources :notifications, only: [ :index ]
