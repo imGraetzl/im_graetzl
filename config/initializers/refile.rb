f69e6287afae91dd82b5983cd48658faa74b6caf
Max Mulatz <klappradla@gmail.com>
2015-09-16 11:58:34 +0200

    remove linebreak from aws config

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -6,7 +6,7 @@ if Rails.env.production?
     access_key_id: ENV['S3_ACCESS_KEY'],
     secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
     region: 'eu-central-1',
-    bucket: ENV['S3_BUCKET'],
+    bucket: ENV['S3_BUCKET']
   }
   Refile.cache = Refile::S3.new(prefix: 'refile/cache', **aws)
   Refile.store = Refile::S3.new(prefix: 'refile/store', **aws)
