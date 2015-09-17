e2440a554796dba9591f1adeb653de87bcc6acc9
Max Mulatz <klappradla@gmail.com>
2015-09-17 20:45:09 +0200

    show more meetings on district pages Close #107

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -1,14 +1,14 @@
 class DistrictsController < ApplicationController
   def index
     @districts = District.all()
-    @meetings = Meeting.basic.upcoming.limit(4)
+    @meetings = Meeting.basic.upcoming.limit(60)
     @map_data = GeoJSONService.call(districts: @districts)
   end
 
   def show
     @district = District.find(params[:id])
     @graetzls = @district.graetzls
-    @meetings = Meeting.basic.upcoming.where(graetzl: @graetzls).limit(4)
+    @meetings = Meeting.basic.upcoming.where(graetzl: @graetzls).limit(60)
     @map_data = GeoJSONService.call(districts: @district, graetzls: @graetzls)
   end
 end
