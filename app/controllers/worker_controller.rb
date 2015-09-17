263f2863370c2f8bda9529df887da5651e6af53b
Tawan Sierek <tawan.sierek@gmail.com>
2015-09-17 23:24:39 +0200

    Close #86 DB backup
    Backup is dumped every day at midgnight by a worker action
    and uploaded to S# bucket im-graetzl-db-backups

--- app/controllers/worker_controller.rb
+++ app/controllers/worker_controller.rb
@@ -1,3 +1,5 @@
+require 'tempfile'
+
 class WorkerController < ApplicationController
   protect_from_forgery except: [ :daily_mail, :weekly_mail ]
 
@@ -19,6 +21,23 @@ class WorkerController < ApplicationController
         SendMailNotificationJob.perform_later(u.id, 'weekly')
       end
       Rails.logger.info "Send weekly mail"
+      render nothing: true, status: :ok
+    else
+      render body: "not allowed", status: :forbidden
+    end
+  end
+
+  def backup
+    if ENV["ALLOW_WORKER"] == 'true' 
+
+      dump = Tempfile.new('dump')
+      dump_cmd = "PGPASSWORD=#{ENV['DB_PASSWORD']} pg_dumpall -h #{ENV['DB_HOST']} -U #{ENV['DB_USERNAME']} -p #{ENV['DB_PORT']} > #{dump.path}"
+      Rails.logger.info dump_cmd
+      Rails.logger.info `#{dump_cmd}`
+      Aws.config[:credentials] = Aws::Credentials.new(ENV['S3_ACCESS_KEY'], ENV['S3_SECRET_ACCESS_KEY'])
+      Aws.config[:region] = 'eu-central-1'
+      s3 = Aws::S3::Resource.new
+      s3.bucket('im-graetzl-db-backups').object("#{ENV['DB_NAME']}_#{Time.now.strftime("%d-%m-%Y-%H:%M")}").upload_file(dump.path)
       render nothing: true, status: :ok
     else
       render body: "not allowed", status: :forbidden
