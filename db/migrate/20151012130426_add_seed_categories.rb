2bbc1e899a9fdf1146a300eb3d5986df759b791f
Max Mulatz <klappradla@gmail.com>
2015-10-12 15:17:15 +0200

    add default categories, assign category for each location (migration )

---
+++ db/migrate/20151012130426_add_seed_categories.rb
@@ -0,0 +1,28 @@
+class AddSeedCategories < ActiveRecord::Migration
+  def up
+    say_with_time 'Adding default :business categories...' do
+      location_categories = [
+        'Kreativwirtschaft / Handwerk',
+        'Wohlbefinden & Gesundheit',
+        'Unternehmen & Start-ups',
+        'Geschäft / Ladenlokal im Grätzl',
+        'Gastronomie',
+        'Lokaler Dienstleister',
+        'Öffentlicher Raum / Sozialer Treffpunkt',
+        'Leerstand',
+        'Sonstige Tätigkeit']
+      location_categories.each do |c|
+        Category.create(name: c, context: Category.contexts[:business])
+      end
+    end
+
+    say_with_time 'Updating existing locations without category...' do
+      default_category = Category.first
+      Location.where(category: nil).each do |l|
+        l.update(category: default_category)
+      end
+    end
+  end
+end
+
+
