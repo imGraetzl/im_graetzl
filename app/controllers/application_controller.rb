2373697cf92513d60e0b571b56cada72ac2732ac
Max Mulatz <klappradla@gmail.com>
2015-10-12 16:55:20 +0200

    remove httpbasic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  # hide staging app from public
-  http_basic_authenticate_with name: 'user', password: 'secret' if Rails.env.production? && !(ENV["ALLOW_WORKER"] == 'true')
 
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || graetzl_path(resource.graetzl)
