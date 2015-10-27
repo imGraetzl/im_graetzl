0a5607025d417bbb598401aa065e212786abf985
Max Mulatz <klappradla@gmail.com>
2015-10-27 21:12:25 +0100

    notification new meeting in graetzl

--- app/jobs/send_mail_notification_job.rb
+++ app/jobs/send_mail_notification_job.rb
@@ -7,6 +7,7 @@ class SendMailNotificationJob < ActiveJob::Base
       user = User.find user_id
       notifications = []
       template_name = nil
+      subject = nil
       case interval
       when "immediate"
         notifications = [ Notification.find(notification_id) ]
@@ -19,36 +20,44 @@ class SendMailNotificationJob < ActiveJob::Base
         template_name = "weekly-daily-mandrill-notifications"
       else
         notifications = []
-        template_name = "summary-notification"
+        #template_name ||= "notification-#{type}"
+        #template_name = "summary-notification-dev"
       end
       return if notifications.empty?
       template_content = []
       default_url_options = Rails.application.config.action_mailer.default_url_options
       vars = [
         { name: "username", content: user.username },
-        { name: "edit_user_url", content: edit_user_url(user, default_url_options) },
+        { name: "edit_user_url", content: edit_user_url(default_url_options) },
         { name: "first_name", content: user.first_name},
         { name: "last_name", content: user.last_name},
         { name: "graetzl_name", content: user.graetzl.name },
+        { name: "graetzl_url", content: graetzl_url(user.graetzl, default_url_options) },
         { name: "interval", content: interval }
       ]
 
       notification_vars = [ ]
+      message_vars = [ ]
       notifications.each do |notification|
         activity = PublicActivity::Activity.find notification.activity_id
         type = Notification::TYPES.select { |k,v| v[:bitmask] == notification.bitmask }.first[0].to_s
+        template_name ||= "notification-#{type}"
         case type
         when "new_meeting_in_graetzl"
+          subject = "Neues Treffen im #{activity.trackable.graetzl.name}"
           notification_vars << {
             "type": "new_meeting_in_graetzl",
             "owner_name": activity.owner.username,
             "owner_url": user_url(activity.owner, default_url_options),
             "meeting_name": activity.trackable.name,
             "meeting_url": graetzl_meeting_url(activity.trackable.graetzl, activity.trackable, default_url_options),
-            "graetzl_name": activity.trackable.graetzl.name,
-            "graetzl_url": graetzl_url(activity.trackable.graetzl, default_url_options)
+            "meeting_starts_at_date": activity.trackable.starts_at_date ? I18n.localize(activity.trackable.starts_at_date, format:'%A %d. %B') : '',
+            "meeting_starts_at_time": activity.trackable.starts_at_time ? I18n.localize(activity.trackable.starts_at_time, format:'%H:%M') : '',
+            "meeting_description": activity.trackable.description.truncate(300, separator: ' ')
           }
         when "new_post_in_graetzl"
+          subject = "Neues Treffen im #{activity.trackable.graetzl.name}"
+          template_name ||= 'summary-notification-dev'
           notification_vars << {
             "type": "new_post_in_graetzl",
             "post_content": activity.trackable.content,
@@ -59,6 +68,8 @@ class SendMailNotificationJob < ActiveJob::Base
             "graetzl_url": graetzl_url(activity.trackable.graetzl, default_url_options)
           }
         when "another_attendee"
+          subject = "Neues Treffen im #{activity.trackable.graetzl.name}"
+          template_name ||= 'summary-notification-dev'
           notification_vars << {
             "type": "another_attendee",
             "meeting_name": activity.trackable.name,
@@ -69,6 +80,8 @@ class SendMailNotificationJob < ActiveJob::Base
             "graetzl_url": graetzl_url(activity.trackable.graetzl, default_url_options)
           }
         when "attendee_left"
+          subject = "Neues Treffen im #{activity.trackable.graetzl.name}"
+          template_name ||= 'summary-notification-dev'
           notification_vars << {
             "type": "attendee_left",
             "meeting_name": activity.trackable.name,
@@ -79,6 +92,8 @@ class SendMailNotificationJob < ActiveJob::Base
             "graetzl_url": graetzl_url(activity.trackable.graetzl, default_url_options)
           }
         when "another_user_comments"
+          subject = "Neues Treffen im #{activity.trackable.graetzl.name}"
+          template_name ||= 'summary-notification-dev'
           notification_vars << {
             "type": "another_user_comments",
             "meeting_name": activity.trackable.name,
@@ -91,6 +106,8 @@ class SendMailNotificationJob < ActiveJob::Base
             "graetzl_url": graetzl_url(activity.trackable.graetzl, default_url_options)
           }
         when "user_comments_users_meeting"
+          subject = "Neues Treffen im #{activity.trackable.graetzl.name}"
+          template_name ||= 'summary-notification-dev'
           notification_vars << {
             "type": "user_comments_users_meeting",
             "meeting_name": activity.trackable.name,
@@ -103,6 +120,8 @@ class SendMailNotificationJob < ActiveJob::Base
             "graetzl_url": graetzl_url(activity.trackable.graetzl, default_url_options)
           }
         when "initiator_comments"
+          subject = "Neues Treffen im #{activity.trackable.graetzl.name}"
+          template_name ||= 'summary-notification-dev'
           notification_vars << {
             "type": "initiator_comments",
             "meeting_name": activity.trackable.name,
@@ -115,6 +134,8 @@ class SendMailNotificationJob < ActiveJob::Base
             "graetzl_url": graetzl_url(activity.trackable.graetzl, default_url_options)
           }
         when "update_of_meeting"
+          subject = "Neues Treffen im #{activity.trackable.graetzl.name}"
+          template_name ||= 'summary-notification-dev'
           translation = {
             :address => "Ort",
             :adress_attributes => "Ort",
@@ -137,6 +158,8 @@ class SendMailNotificationJob < ActiveJob::Base
             "graetzl_url": graetzl_url(activity.trackable.graetzl, default_url_options)
           }
         when "new_wall_comment"
+          subject = "Neues Treffen im #{activity.trackable.graetzl.name}"
+          template_name ||= 'summary-notification-dev'
           notification_vars << {
             "type": "new_wall_comment",
             "comment_url": graetzl_user_url(activity.trackable.graetzl, activity.trackable, default_url_options) + "#comment-#{activity.recipient.id}",
@@ -145,6 +168,8 @@ class SendMailNotificationJob < ActiveJob::Base
             "owner_url": user_url(activity.owner, default_url_options)
           }
         when "cancel_of_meeting"
+          subject = "Neues Treffen im #{activity.trackable.graetzl.name}"
+          template_name ||= 'summary-notification-dev'
           notification_vars << {
             "type": "cancel_of_meeting",
             "meeting_name": activity.trackable.name,
@@ -154,6 +179,8 @@ class SendMailNotificationJob < ActiveJob::Base
             "owner_url": user_url(activity.owner, default_url_options)
           }
         when "approve_of_location"
+          subject = "Neues Treffen im #{activity.trackable.graetzl.name}"
+          template_name ||= 'summary-notification-dev'
           notification_vars << {
             "type": "approve_of_location",
             "location_name": activity.trackable.name,
@@ -164,6 +191,9 @@ class SendMailNotificationJob < ActiveJob::Base
 
       message = {
         to: [ { email: user.email } ],
+        from_email: Rails.configuration.x.mandril_from_email,
+        from_name: Rails.configuration.x.mandril_from_name,
+        subject: subject,
         merge_vars: [
           rcpt: user.email,
           vars: vars + [
