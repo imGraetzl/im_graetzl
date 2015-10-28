2f0354a9175e144445b1fb3cf39626b3cecbacab
Max Mulatz <klappradla@gmail.com>
2015-10-28 15:37:35 +0100

    notification another user comments

--- app/jobs/send_mail_notification_job.rb
+++ app/jobs/send_mail_notification_job.rb
@@ -66,7 +66,6 @@ class SendMailNotificationJob < ActiveJob::Base
           }
         when "another_attendee"
           subject = 'Neuer Teilnehmer an deinem Treffen'
-          template_name ||= 'summary-notification-dev'
           notification_vars << {
             "type": "another_attendee",
             "meeting_name": activity.trackable.name,
@@ -77,7 +76,6 @@ class SendMailNotificationJob < ActiveJob::Base
           }
         when "attendee_left"
           subject = 'Absage eines Teilnehmers an deinem Treffen'
-          template_name ||= 'summary-notification-dev'
           notification_vars << {
             "type": "attendee_left",
             "meeting_name": activity.trackable.name,
@@ -87,8 +85,7 @@ class SendMailNotificationJob < ActiveJob::Base
             "graetzl_name": activity.trackable.graetzl.name,
           }
         when "another_user_comments"
-          subject = "Neues Treffen im #{activity.trackable.graetzl.name}"
-          template_name ||= 'summary-notification-dev'
+          subject = 'Neuer Kommentar in einem Treffen'
           notification_vars << {
             "type": "another_user_comments",
             "meeting_name": activity.trackable.name,
@@ -97,12 +94,11 @@ class SendMailNotificationJob < ActiveJob::Base
             "owner_name": activity.owner.username,
             "owner_url": user_url(activity.owner, default_url_options),
             "meeting_url": graetzl_meeting_url(activity.trackable.graetzl, activity.trackable, default_url_options),
-            "graetzl_name": activity.trackable.graetzl.name,
-            "graetzl_url": graetzl_url(activity.trackable.graetzl, default_url_options)
+            "graetzl_name": activity.trackable.graetzl.name
           }
         when "user_comments_users_meeting"
           subject = "Neues Treffen im #{activity.trackable.graetzl.name}"
-          template_name ||= 'summary-notification-dev'
+          template_name = 'summary-notification-dev'
           notification_vars << {
             "type": "user_comments_users_meeting",
             "meeting_name": activity.trackable.name,
