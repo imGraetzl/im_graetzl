dd9d41369087b58e714b3dd6c789ce3bbcdb11e7
Max Mulatz <klappradla@gmail.com>
2015-10-29 15:37:49 +0100

    pass activity to job, not just id

--- app/jobs/create_website_notifications_job.rb
+++ app/jobs/create_website_notifications_job.rb
@@ -1,9 +1,10 @@
 class CreateWebsiteNotificationsJob < ActiveJob::Base
   queue_as :default
 
-  def perform(activity_id)
+  #def perform(activity_id)
+  def perform(activity)
     #ActiveRecord::Base.connection_pool.with_connection do
-      activity = PublicActivity::Activity.find(activity_id)
+      #activity = PublicActivity::Activity.find(activity_id)
       Notification.broadcast(activity)
     #end
   end
