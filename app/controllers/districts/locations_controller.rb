94923d9586020a67a92c431fd5dc5e26155dd0c7
Max Mulatz <klappradla@gmail.com>
2015-11-09 11:46:00 +0100

    update district locations view

--- app/controllers/districts/locations_controller.rb
+++ app/controllers/districts/locations_controller.rb
@@ -7,7 +7,7 @@ class Districts::LocationsController < ApplicationController
 
   def index
     @district = District.find(params[:district_id])
-    @locations = @district.locations.approved.page(params[:page]).per(5)
+    @locations = @district.locations.approved.page(params[:page]).per(19)
     @map_data = GeoJSONService.call(districts: @district, graetzls: @district.graetzls) unless request.xhr?
   end
 end
