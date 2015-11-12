123889b796ab76dc0823662b457ef20f0bdfdbfc
Max Mulatz <klappradla@gmail.com>
2015-11-12 12:37:36 +0100

    update sitemap with priorities

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -3,9 +3,9 @@ SitemapGenerator::Sitemap.default_host = 'http://www.imgraetzl.at'
 
 SitemapGenerator::Sitemap.create do
   # Districts
-  add districts_path
+  add districts_path, changefreq: 'always', priority: 0.7
   District.find_each do |district|
-    add district_path(district), lastmod: district.updated_at, changefreq: 'always'
+    add district_path(district), changefreq: 'always', priority: 0.7
     add district_locations_path(district) unless district.locations.empty?
     add district_meetings_path(district) unless district.meetings.empty?
   end
@@ -15,14 +15,14 @@ SitemapGenerator::Sitemap.create do
 
   # Graetzls
   Graetzl.find_each do |graetzl|
-    add graetzl_path(graetzl), lastmod: graetzl.updated_at, changefreq: 'always'
+    add graetzl_path(graetzl), changefreq: 'always', priority: 0.7
 
     # Locations
     locations = graetzl.locations.approved
     unless locations.empty?
       add graetzl_locations_path(graetzl)
       locations.find_each do |location|
-        add graetzl_location_path(graetzl, location)
+        add graetzl_location_path(graetzl, location), priority: 0.7
       end
     end
 
@@ -31,15 +31,22 @@ SitemapGenerator::Sitemap.create do
     unless meetings.empty?
       add graetzl_meetings_path(graetzl)
       meetings.find_each do |meeting|
-        add graetzl_meeting_path(graetzl, meeting)
+        add graetzl_meeting_path(graetzl, meeting), priority: 0.7
       end
     end
   end
 
   # Info Pages
-  add info_agb_path, changefreq: 'never'
-  add info_datenschutz_path, changefreq: 'never'
-  add info_impressum_path, changefreq: 'never'
-  add info_infos_zum_graetzlzuckerl_path, changefreq: 'never'
-  add info_fragen_und_antworten_path, changefreq: 'never'
+  add info_agb_path, changefreq: 'never', priority: 0.3
+  add info_datenschutz_path, changefreq: 'never', priority: 0.3
+  add info_impressum_path, changefreq: 'never', priority: 0.3
+  add info_infos_zum_graetzlzuckerl_path, changefreq: 'never', priority: 0.3
+  add info_fragen_und_antworten_path, changefreq: 'never', priority: 0.3
+
+  # Registration (only get paths)
+  add new_user_session_path, changefreq: 'never', priority: 0.3
+  add new_password_path, changefreq: 'never', priority: 0.3
+  add edit_password_path, changefreq: 'never', priority: 0.3
+  add new_confirmation_path, changefreq: 'never', priority: 0.3
+  add new_registration_path, changefreq: 'never', priority: 0.3
 end
