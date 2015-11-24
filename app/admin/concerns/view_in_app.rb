b271f0fe489db0b0235e383de37887bdbcd0afd9
Max Mulatz <klappradla@gmail.com>
2015-11-24 11:16:18 +0100

    disable admin view_in_app module

--- app/admin/concerns/view_in_app.rb
+++ app/admin/concerns/view_in_app.rb
@@ -1,15 +1,15 @@
 # Shared method for ActiveAdmin resources
-module ViewInApp
-  extend ActiveSupport::Concern
-
-  def self.included(base)
-    # Shared action_item (use 'send' due to 'action_item' is a private method)
-    base.send(:action_item, :view_in_app, only: :show) do
-      if resource.respond_to?(:graetzl)
-        link_to 'In App ansehen', [resource.graetzl, resource]
-      elsif resource.is_a?(Graetzl)
-        link_to 'In App ansehen', resource
-      end
-    end
-  end
-end
+# module ViewInApp
+#   extend ActiveSupport::Concern
+#
+#   def self.included(base)
+#     # Shared action_item (use 'send' due to 'action_item' is a private method)
+#     base.send(:action_item, :view_in_app, only: :show) do
+#       if resource.respond_to?(:graetzl)
+#         link_to 'In App ansehen', [resource.graetzl, resource]
+#       elsif resource.is_a?(Graetzl)
+#         link_to 'In App ansehen', resource
+#       end
+#     end
+#   end
+# end
