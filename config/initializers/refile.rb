96ded96c4cbf12099af98f7a4631c44f468bcf44
Max Mulatz <klappradla@gmail.com>
2015-11-27 12:49:08 +0100

    update refile initializer to use same aws credentials as rake tasks

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -3,8 +3,8 @@ require 'refile/s3'
 if Rails.env.production? || Rails.env.staging?
 
   aws = {
-    access_key_id: ENV['S3_ACCESS_KEY'],
-    secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_KEY'],
     region: 'eu-central-1',
     bucket: ENV['S3_BUCKET']
   }
