75188d0a303355e5e144cb910d3dd5c462412ce9
Max Mulatz <klappradla@gmail.com>
2016-01-20 16:20:59 +0100

    edit old migration to match new db schema

--- db/migrate/20160104143043_add_key_to_notifications.rb
+++ db/migrate/20160104143043_add_key_to_notifications.rb
@@ -2,13 +2,6 @@ class AddKeyToNotifications < ActiveRecord::Migration
   def up
     add_column :notifications, :key, :string
 
-    say_with_time 'Updating existing notification records to have a key value' do
-      Notification.find_each do |n|
-        key = Notification::TYPES.select{|k,v| v[:bitmask] == n.bitmask}.first[0]
-        n.update(key: key)
-      end
-    end
-
     change_column_null :notifications, :key, false
   end
 
