6bbb0a61d5a2318986fa3e7377a240d492e04c1f
Max Mulatz <klappradla@gmail.com>
2016-01-28 12:58:25 +0100

    reenable notification center

--- app/assets/javascripts/components/notificationCenter.js
+++ app/assets/javascripts/components/notificationCenter.js
@@ -8,7 +8,7 @@ APP.components.notificatonCenter = (function() {
         $(window).load(function() {
             $notificationsContainer = $("[data-behavior='notifications-container']");
             $notificationsTrigger = $("[data-behavior='notifications-trigger']");
-            // if ($notificationsContainer.exists()) setup();
+            if ($notificationsContainer.exists()) setup();
         });
     }
 
