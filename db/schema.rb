d764ee9ae37fa13ff7e5bbab521aaf41cc6dae26
Max Mulatz <klappradla@gmail.com>
2016-01-28 11:22:51 +0100

    update user enabled_website_notifications default

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114165459) do
+ActiveRecord::Schema.define(version: 20160128101206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -294,20 +294,20 @@ ActiveRecord::Schema.define(version: 20160114165459) do
     t.string   "username",                      limit: 255
     t.string   "first_name",                    limit: 255
     t.string   "last_name",                     limit: 255
-    t.boolean  "newsletter",                                default: false, null: false
     t.integer  "graetzl_id"
     t.string   "avatar_id"
-    t.integer  "enabled_website_notifications",             default: 2047
     t.integer  "role"
     t.string   "avatar_content_type"
-    t.integer  "immediate_mail_notifications",              default: 2013
-    t.integer  "daily_mail_notifications",                  default: 0
-    t.integer  "weekly_mail_notifications",                 default: 0
     t.string   "slug"
     t.string   "cover_photo_id"
     t.string   "cover_photo_content_type"
     t.text     "bio"
     t.string   "website"
+    t.integer  "weekly_mail_notifications",                 default: 0
+    t.integer  "daily_mail_notifications",                  default: 0
+    t.integer  "immediate_mail_notifications",              default: 2013
+    t.integer  "enabled_website_notifications"
+    t.boolean  "newsletter",                                default: false, null: false
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
