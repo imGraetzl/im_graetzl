3cf1b3c243ab213cba3cbd371fe6db5d3a636b4f
Max Mulatz <klappradla@gmail.com>
2016-02-15 13:03:35 +0100

    remove unused associations

--- app/models/going_to.rb
+++ app/models/going_to.rb
@@ -1,14 +1,8 @@
 class GoingTo < ActiveRecord::Base
   include PublicActivity::Common
 
-  # associations
   belongs_to :user
   belongs_to :meeting
-  has_many :activities, as: :trackable, class_name: 'PublicActivity::Activity', dependent: :destroy
 
   enum role: { attendee: 0, initiator: 1 }
-
-  def button_type
-    
-  end
 end
--- app/models/location_ownership.rb
+++ app/models/location_ownership.rb
@@ -1,34 +1,7 @@
 class LocationOwnership < ActiveRecord::Base
-  # scopes
-  #scope :all_pending, -> { where(state: [states[:pending], states[:requested]]) }
-  
-  # associations
+
   belongs_to :user
   belongs_to :location
 
   enum state: { basic: 0, pending: 1 }
-
-  # states
-  # include AASM
-  # enum state: { pending: 0, requested: 1, approved: 2, rejected: 3 }
-  # aasm column: :state do
-  #   state :pending, initial: true
-  #   state :requested
-  #   state :approved
-  #   state :rejected
-
-  #   event :approve, after: :notify_user do
-  #     transitions from: :pending, to: :approved, guard: :location_approved?
-  #     transitions from: :requested, to: :approved, guard: :location_approved?
-  #   end
-  # end
-
-  # private
-
-  #   def notify_user    
-  #   end
-
-  #   def location_approved?
-  #     location.managed?
-  #   end
 end
