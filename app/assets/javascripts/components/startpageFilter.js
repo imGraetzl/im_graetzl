7c061289b65fc1f99e20096ee2df222287ab4629
ptilg <peter.tilg@gmail.com>
2016-02-19 15:31:03 +0100

    new card styles and masonry layout

--- app/assets/javascripts/components/startpageFilter.js
+++ app/assets/javascripts/components/startpageFilter.js
@@ -1,11 +1,21 @@
 APP.components.startpageFilter = (function() {
 
-    var $createTrigger, $createContainer;
-
     function init() {
 
 
         $('[data-behavior=createTrigger]').jqDropdown('attach', '[data-behavior=createContainer]');
+
+
+        var $grid = $('.cards-container').masonry({
+            percentPosition: true
+        });
+
+        setInterval(function() {
+            $grid.masonry('layout');
+        }, 1000);
+
+
+
     }
 
 
