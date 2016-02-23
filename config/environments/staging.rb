79a538f1a8930538ee730285dc49ae3e0688e46c
Max Mulatz <klappradla@gmail.com>
2016-02-23 18:00:18 +0100

    try proc solution

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -58,7 +58,15 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = ENV['ASSET_HOST']
+  config.action_controller.asset_host = proc {|source|
+    if File.extname(source) =~ /\.(svg|woff|woff2|ttf|otf|eot)/ || source =~ FONT_FILE_REGEX
+      "https://staging.imgraetzl.at"
+    else
+      ENV['ASSET_HOST']
+    end
+  }
+
+  ENV['ASSET_HOST']
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
