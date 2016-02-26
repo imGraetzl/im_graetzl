a5a19f63bcef1a617735acb1b04b0367fb0ba443
Max Mulatz <klappradla@gmail.com>
2016-02-26 16:28:19 +0100

    add avatar_service to serve avatar images in mail via cdn fixes #199

--- app/models/notifications/new_location_post.rb
+++ app/models/notifications/new_location_post.rb
@@ -21,7 +21,7 @@ class Notifications::NewLocationPost < Notification
       post_title: activity.trackable.title,
       post_content: activity.trackable.content.truncate(255, separator: ' '),
       owner_name: activity.trackable.author.name,
-      owner_avatar_url: ApplicationController.helpers.attachment_url(activity.trackable.author, :avatar, :fill, 40, 40, host: "http://#{DEFAULT_URL_OPTIONS[:host]}") || ApplicationController.helpers.image_url('avatar/location/40x40.png', host: "http://#{DEFAULT_URL_OPTIONS[:host]}"),
+      owner_avatar_url: Notifications::AvatarService.new(activity.trackable.author).call,
       owner_url: graetzl_location_url(activity.trackable.author.graetzl, activity.trackable.author, DEFAULT_URL_OPTIONS),
       post_url: graetzl_location_url(activity.trackable.graetzl, activity.trackable.author, DEFAULT_URL_OPTIONS),
     }
