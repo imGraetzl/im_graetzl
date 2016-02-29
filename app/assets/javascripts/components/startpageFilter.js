1c81a47fb1824bff6519775662de02f89194277c
ptilg <peter.tilg@gmail.com>
2016-02-29 12:30:17 +0100

    removed commentform

--- app/assets/javascripts/components/startpageFilter.js
+++ app/assets/javascripts/components/startpageFilter.js
@@ -1,23 +1,12 @@
 APP.components.startpageFilter = (function() {
 
     function init() {
-
-
         $('[data-behavior=createTrigger]').jqDropdown('attach', '[data-behavior=createContainer]');
 
-
-        var $grid = $('.cards-container').masonry({
-            percentPosition: true
-        });
-
-        setInterval(function() {
-            $grid.masonry('layout');
-        }, 1000);
-
-
-
+        $(window).on("load", function() {
+            $('.cards-container').masonry();
+        })
     }
-
 
     return {
         init: init
