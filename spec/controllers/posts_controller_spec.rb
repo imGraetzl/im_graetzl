6cc20c32aadb011a2c2de2c1eb1aca5d47b681d9
Max Mulatz <klappradla@gmail.com>
2016-03-15 15:14:56 +0100

    specs for posts controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,121 +1,92 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-  # let(:user) { create(:user) }
-  #
-  # describe 'GET show' do
-  #   context 'when logged out' do
-  #     it 'redirects to login_page' do
-  #       get :show, graetzl_id: 'graetzl-slug', id: 'post-slug'
-  #       expect(response).to render_template(session[:new])
-  #     end
-  #   end
-  #
-  #   context 'when logged in' do
-  #     before { sign_in user }
-  #
-  #     context 'when author user' do
-  #       let(:post) { create(:post) }
-  #
-  #       context 'when html request' do
-  #         before { get :show, graetzl_id: post.graetzl, id: post }
-  #
-  #         it 'assigns @post' do
-  #           expect(assigns(:post)).to eq post
-  #         end
-  #
-  #         it 'assigns @comments' do
-  #           expect(assigns(:comments)).to be
-  #         end
-  #
-  #         it 'renders show.html' do
-  #           expect(response['Content-Type']).to eq 'text/html; charset=utf-8'
-  #           expect(response).to render_template(:show)
-  #         end
-  #       end
-  #
-  #       context 'when js request' do
-  #         before { xhr :get, :show, graetzl_id: post.graetzl, id: post }
-  #
-  #         it 'assigns @post' do
-  #           expect(assigns(:post)).to eq post
-  #         end
-  #
-  #         it 'assigns @comments' do
-  #           expect(assigns(:comments)).to be
-  #         end
-  #
-  #         it 'renders show.js' do
-  #           expect(response['Content-Type']).to eq 'text/javascript; charset=utf-8'
-  #           expect(response).to render_template(:show)
-  #         end
-  #       end
-  #     end
-  #
-  #     context 'when author location' do
-  #       let(:post) { create(:post, author: create(:location)) }
-  #       before do
-  #         request.env['HTTP_REFERER'] = 'where_i_came_from'
-  #         get :show, graetzl_id: post.graetzl, id: post
-  #       end
-  #
-  #       it 'redirects back to previous page' do
-  #         expect(response).to redirect_to 'where_i_came_from'
-  #       end
-  #
-  #       it 'shows flash message' do
-  #         expect(flash[:notice]).to be_present
-  #       end
-  #     end
-  #   end
-  # end
-  #
-  # describe 'DELETE destroy' do
-  #   let(:graetzl) { create(:graetzl) }
-  #   let!(:post) { create(:post, graetzl: graetzl) }
-  #
-  #   context 'when logged out' do
-  #
-  #     it 'redirects js request to login_page' do
-  #       xhr :delete, :destroy, id: post
-  #       expect(response).to render_template(session[:new])
-  #     end
-  #
-  #     it 'redirects html request to login_page' do
-  #       delete :destroy, id: post
-  #       expect(response).to render_template(session[:new])
-  #     end
-  #   end
-  #
-  #   context 'when logged in' do
-  #     before { sign_in create(:user) }
-  #
-  #     context 'when js request' do
-  #       it 'deletes record' do
-  #         expect {
-  #           xhr :delete, :destroy, id: post
-  #         }.to change(Post, :count).by(-1)
-  #       end
-  #
-  #       it 'renders destroy.js' do
-  #         xhr :delete, :destroy, id: post
-  #         expect(response['Content-Type']).to include('text/javascript')
-  #         expect(response).to render_template('posts/destroy')
-  #       end
-  #     end
-  #
-  #     context 'when html request' do
-  #       it 'deletes post record' do
-  #         expect{
-  #           delete :destroy, id: post
-  #         }.to change{Post.count}.by(-1)
-  #       end
-  #
-  #       it 'redirects to graetzl' do
-  #         delete :destroy, id: post
-  #         expect(response).to redirect_to graetzl
-  #       end
-  #     end
-  #   end
-  # end
+  let(:graetzl) { create :graetzl }
+
+  describe 'GET index' do
+    let!(:old_user_posts) { create_list :user_post, 15, graetzl: graetzl }
+    let!(:new_user_posts) { create_list :user_post, 15, graetzl: graetzl }
+
+    context 'when html request' do
+      before { get :index, graetzl_id: graetzl }
+
+      it 'assigns @graetzl' do
+        expect(assigns :graetzl).to eq graetzl
+      end
+
+      it 'assigns @map_data' do
+        expect(assigns :map_data).to be
+      end
+
+      it 'assigns newest @posts' do
+        expect(assigns :posts).to match_array new_user_posts
+      end
+
+      it 'renders index.html' do
+        expect(response['Content-Type']).to eq 'text/html; charset=utf-8'
+        expect(response).to render_template :index
+      end
+    end
+    context 'when js request' do
+      before { xhr :get, :index, graetzl_id: graetzl, page: 2 }
+
+      it 'assigns @graetzl' do
+        expect(assigns :graetzl).to eq graetzl
+      end
+
+      it 'does not assign @map_data' do
+        expect(assigns :map_data).not_to be
+      end
+
+      it 'assigns older @posts' do
+        expect(assigns :posts).to match_array old_user_posts
+      end
+
+      it 'renders index.js' do
+        expect(response['Content-Type']).to eq 'text/javascript; charset=utf-8'
+        expect(response).to render_template :index
+      end
+    end
+
+    describe 'DELETE destroy' do
+      let!(:post) { create :user_post, graetzl: graetzl }
+
+      context 'when logged out' do
+        it 'redirects to login with flash' do
+          delete :destroy, id: post
+          expect(response).to redirect_to new_user_session_path
+          expect(flash[:alert]).to be_present
+        end
+      end
+      context 'when logged in' do
+        let(:user) { create :user }
+        before { sign_in user }
+
+        context 'when html request' do
+          it 'deletes record' do
+            expect{
+              delete :destroy, id: post
+            }.to change{Post.count}.by -1
+          end
+
+          it 'redirects to graetzl' do
+            delete :destroy, id: post
+            expect(response).to redirect_to graetzl
+          end
+        end
+        context 'when js request' do
+          it 'deletes record' do
+            expect{
+              xhr :delete, :destroy, id: post
+            }.to change{Post.count}.by -1
+          end
+
+          it 'render destroy.js' do
+            xhr :delete, :destroy, id: post
+            expect(response).to render_template :destroy
+          end
+        end
+      end
+    end
+  end
 end
