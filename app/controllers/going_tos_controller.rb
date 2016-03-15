9adf09426ceb5c8ec3508cc886a902409421ef51
Max Mulatz <klappradla@gmail.com>
2016-03-15 16:35:42 +0100

    fix going tos controller specs, rendering and activities

--- app/controllers/going_tos_controller.rb
+++ app/controllers/going_tos_controller.rb
@@ -5,22 +5,10 @@ class GoingTosController < ApplicationController
     @meeting = Meeting.find(params[:meeting_id])
     @meeting.users << current_user
     @meeting.create_activity :go_to, owner: current_user
-
-    respond_to do |format|
-      format.js
-      format.json { render json: @meeting }
-    end
   end
 
   def destroy
-    going_to = GoingTo.find(params[:id])
+    going_to = GoingTo.find(params[:id]).destroy
     @meeting = going_to.meeting
-    going_to.destroy
-    @meeting.create_activity :left, owner: current_user
-
-    respond_to do |format|
-      format.js
-      format.json { render json: @meeting }
-    end
   end
 end
--- spec/controllers/going_tos_controller_spec.rb
+++ spec/controllers/going_tos_controller_spec.rb
@@ -1,98 +1,76 @@
 require 'rails_helper'
 
 RSpec.describe GoingTosController, type: :controller do
-  let(:graetzl) { create(:graetzl) }
-  let(:meeting) { create(:meeting, graetzl: graetzl) }
-  let(:user) { create(:user) }
-
-  shared_examples :a_successful_request do
-    it 'assigns @meeting' do
-      expect(assigns(:meeting)).to eq(meeting)
-    end
-  end
-
-  shared_examples :an_unauthenticated_request do
-    it 'redirects to login' do
-      expect(response).to render_template(session[:new])
-    end
-  end
+  let(:graetzl) { create :graetzl }
 
   describe 'POST create' do
-    context 'when not logged in' do
-      before { xhr :post, :create, meeting_id: meeting.id }
-      it_behaves_like :an_unauthenticated_request
-    end
+    let(:meeting) { create :meeting, graetzl: graetzl }
 
+    context 'when logged out' do
+      it 'returns 401 unauthorized' do
+        xhr :post, :create, meeting_id: meeting
+        expect(response).to have_http_status 401
+      end
+    end
     context 'when logged in' do
+      let(:user) { create :user }
+
       before { sign_in user }
 
-      it 'creates new GoingTo record' do
-        expect {
-          xhr :post, :create, meeting_id: meeting.id
-          }.to change(GoingTo, :count).by(1)
+      it 'assigns @meeting' do
+        xhr :post, :create, meeting_id: meeting
+        expect(assigns :meeting).to eq meeting
       end
 
-      describe 'meeting' do
-        before { xhr :post, :create, meeting_id: meeting.id }
+      it 'creates new going_to record' do
+        expect {
+          xhr :post, :create, meeting_id: meeting
+        }.to change{GoingTo.count}.by 1
+      end
 
-        it_behaves_like :a_successful_request
+      it 'logs activity' do
+        expect{
+          xhr :post, :create, meeting_id: meeting
+        }.to change{Activity.count}.by 1
+      end
 
-        it 'renders create.js' do
-          expect(response).to render_template('going_tos/create')
-          expect(response.header['Content-Type']).to include('text/javascript')
-        end
-
-        it 'creates GoingTo with role attendee' do
-          expect(GoingTo.last).to have_attributes(
-            user: user,
-            role: 'attendee')
-        end
-
-        it 'adds meeting to user' do
-          expect(user.meetings).to include(meeting)
-        end
-
-        it 'adds user to meeting' do
-          expect(meeting.users).to include(user)
-        end
+      it 'renders create.js' do
+        xhr :post, :create, meeting_id: meeting
+        expect(response['Content-Type']).to eq 'text/javascript; charset=utf-8'
+        expect(response).to render_template :create
       end
     end
   end
 
   describe 'DELETE destroy' do
-    let!(:going_to) { create(:going_to, user: user, meeting: meeting) }
+    let(:user) { create :user }
+    let(:meeting) { create :meeting }
+    let!(:going_to) { create :going_to, :attendee, user: user, meeting: meeting }
 
-    context 'when not logged in' do
-      before { xhr :delete, :destroy, id: going_to.id }
-      it_behaves_like :an_unauthenticated_request
+    context 'when logged out' do
+      it 'returns 401 unauthorized' do
+        xhr :delete, :destroy, id: going_to
+        expect(response).to have_http_status 401
+      end
     end
-
     context 'when logged in' do
       before { sign_in user }
 
-      it 'removes GoingTo' do
-        expect {
-          xhr :delete, :destroy, id: going_to.id
-          }.to change(GoingTo, :count).by(-1)
+      it 'deletes going to record' do
+        expect{
+          xhr :delete, :destroy, id: going_to
+        }.to change{GoingTo.count}.by -1
       end
 
-      describe 'meeting' do
-        before { xhr :delete, :destroy, id: going_to.id }
+      it 'assigns @meeting' do
+        xhr :delete, :destroy, id: going_to
+        expect(assigns :meeting).to eq meeting
+      end
 
-        it_behaves_like :a_successful_request
-
-        it 'renders destroy.js' do
-          expect(response).to render_template('going_tos/destroy')
-          expect(response.header['Content-Type']).to include('text/javascript')
-        end
-
-        it 'removes user from meeting' do
-          expect(meeting.users).not_to include(user)
-        end
-
-        it 'removes meeting from user' do
-          expect(user.meetings).not_to include(meeting)
-        end
+      it 'renders destroy.js' do
+        xhr :delete, :destroy, id: going_to
+        expect(response['Content-Type']).to eq 'text/javascript; charset=utf-8'
+        expect(response).to render_template :destroy
       end
     end
   end
