7de56e864fd294ddf00cede6286cf8f17219b3bb
ptilg <peter.tilg@gmail.com>
2016-03-21 13:23:39 +0100

    mobile selectnavigation for strream

--- app/assets/javascripts/components/masonryFilterGrid.js
+++ app/assets/javascripts/components/masonryFilterGrid.js
@@ -5,14 +5,33 @@ APP.components.masonryFilterGrid = (function() {
     function init() {
         $grid =  $('.cards-container');
         $('[data-behavior=createTrigger]').jqDropdown('attach', '[data-behavior=createContainer]');
-
-        $(window).on("load", function() {
-            $grid.masonry();
-        });
+        $(window).on("load", function() { $grid.masonry(); });
+        createMobileNav();
     }
 
     function adjustNewCards() {
         $grid.masonry('appended', $(".cardBox").not('[style]'));
+    }
+
+    function createMobileNav() {
+        var $dropdown = $(".filter-stream .input-select select");
+        $(".filter-stream .iconfilter").not('.createentry').each(function() {
+            var $this = $(this),
+                link = $this.prop('href'),
+                txt = $this.find('.txt').text();
+
+            $dropdown.append(getOption());
+            $dropdown.on('change', function() {
+                document.location.href = $dropdown.val();
+            });
+
+            function getOption() {
+                if($this.hasClass('active'))
+                    return '<option selected value="'+ link +'">'+ txt +'</option>';
+                return '<option value="'+ link +'">'+ txt +'</option>';
+            }
+
+        })
     }
 
     return {
