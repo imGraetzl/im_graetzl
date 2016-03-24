a4573ae88f1d98d8badec3212b1ae3ebbbd45238
Max Mulatz <klappradla@gmail.com>
2016-03-24 16:07:11 +0100

    fixes on notification paths

--- app/models/notifications/new_location_post.rb
+++ app/models/notifications/new_location_post.rb
@@ -22,7 +22,7 @@ class Notifications::NewLocationPost < Notification
       owner_name: activity.trackable.author.name,
       owner_avatar_url: Notifications::AvatarService.new(activity.trackable.author).call,
       owner_url: graetzl_location_url(activity.trackable.author.graetzl, activity.trackable.author, DEFAULT_URL_OPTIONS),
-      post_url: graetzl_location_url(activity.trackable.graetzl, activity.trackable.author, anchor: ApplicationController.helpers.dom_id(activity.trackable)),
+      post_url: graetzl_location_url(activity.trackable.graetzl, activity.trackable.author, anchor: ApplicationController.helpers.dom_id(activity.trackable), host: DEFAULT_URL_OPTIONS[:host]),
     }
   end
 
