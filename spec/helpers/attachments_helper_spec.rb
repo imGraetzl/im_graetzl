fa819b19819994d00847f3c6da3293372e2ecf4c
Max Mulatz <klappradla@gmail.com>
2016-05-16 17:00:59 +0200

    rename images_helper to attachments_helper, use for simple cover_photos, too (fixes #194)

--- spec/helpers/attachments_helper_spec.rb
+++ spec/helpers/attachments_helper_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe ImagesHelper, type: :helper do
+RSpec.describe AttachmentsHelper, type: :helper do
   describe '.avatar_for' do
     context 'when passing a user' do
       let(:fallback_regex) { /<img.*"attachment user avatar img-round fallback".*\/>/ }
       let(:avatar_regex) { /<img.*"attachment user avatar img-round".*\/>/ }
       let(:resource) { build :user }
 
-      it 'returns a 200 by 200 fallback image if no uploaded image' do
+      it 'returns fallback if no uploaded image' do
         expect(helper.avatar_for resource).to match fallback_regex
       end
 
-      it 'returns a 200 by 200 avatar image' do
+      it 'returns avatar image' do
         allow(resource).to receive(:avatar_id){123456789}
         expect(helper.avatar_for resource).to match avatar_regex
       end
@@ -21,13 +21,31 @@ RSpec.describe ImagesHelper, type: :helper do
       let(:avatar_regex) { /<img.*"attachment location avatar img-square".*\/>/ }
       let(:resource) { build :location }
 
-      it 'returns a 200 by 200 fallback image if no uploaded image' do
+      it 'returns fallback image if no uploaded image' do
         expect(helper.avatar_for resource).to match fallback_regex
       end
 
-      it 'returns a 200 by 200 avatar image' do
+      it 'returns avatar image' do
         allow(resource).to receive(:avatar_id){123456789}
         expect(helper.avatar_for resource).to match avatar_regex
+      end
+    end
+  end
+  describe '.cover_photo_for' do
+    let(:fallback_regex) { /<img.*"attachment (user|location) cover_photo coverImg fallback".*\/>/ }
+    let(:cover_photo_regex) { /<img.*"attachment (user|location) cover_photo coverImg".*\/>/ }
+    let(:resource) { build :user }
+
+    context 'when photo exists' do
+      before { allow(resource).to receive(:cover_photo_id){12345678} }
+
+      it 'returns image' do
+        expect(helper.cover_photo_for resource).to match cover_photo_regex
+      end
+    end
+    context 'when no photo' do
+      it 'returns fallback' do
+        expect(helper.cover_photo_for resource).to match fallback_regex
       end
     end
   end
