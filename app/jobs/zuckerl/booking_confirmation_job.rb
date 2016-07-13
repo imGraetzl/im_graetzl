eca3748f86a685d0d69c7a1f4847eb9707090e64
Max Mulatz <klappradla@gmail.com>
2016-07-13 12:04:20 +0200

    fix typo

--- app/jobs/zuckerl/booking_confirmation_job.rb
+++ app/jobs/zuckerl/booking_confirmation_job.rb
@@ -4,7 +4,7 @@ class Zuckerl::BookingConfirmationJob < ActiveJob::Base
   def perform(zuckerl)
     Rails.logger.info "BookingConfirmationJob start at: #{Time.now}"
     ActiveRecord::Base.connection_pool.with_connection do
-      mandrill_mail = Zuckerl::BookingConfirmationMail.new zucker
+      mandrill_mail = Zuckerl::BookingConfirmationMail.new zuckerl
       mandrill_mail.deliver
     end
   end
