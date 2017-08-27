0c2a1813bbafded230d8b01d336ada1ab56514c9
Nikola Šantić <nikola@readcubes-MacBook-Pro.local>
2017-08-27 17:25:15 +0200

    Added Procfile and Puma config

---
+++ config/puma.rb
@@ -0,0 +1,14 @@
+workers (ENV['WEB_CONCURRENCY'] || 1).to_i
+threads_count = (ENV['MAX_THREADS'] || 4).to_i
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+  ArticlesConnectionBase.custom_establish_connection
+end
