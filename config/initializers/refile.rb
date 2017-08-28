4daef1778dfad5f4ab31439725e4c05935b7e926
Nikola Šantić <nikola@readcubes-MacBook-Pro.local>
2017-08-28 17:02:45 +0200

    Separated assets CDN and uploads CDN

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -6,10 +6,10 @@ if Rails.env.production? || Rails.env.staging?
     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     secret_access_key: ENV['AWS_SECRET_KEY'],
     region: 'eu-central-1',
-    bucket: ENV['S3_BUCKET']
+    bucket: ENV['UPLOADS_BUCKET']
   }
   Refile.cache = Refile::S3.new(prefix: 'refile/cache', **aws)
   Refile.store = Refile::S3.new(prefix: 'refile/store', **aws)
-  Refile.cdn_host = ENV['ASSET_HOST']
+  Refile.cdn_host = ENV['UPLOADS_CDN']
 
 end
