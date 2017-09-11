63c9f0f8cb8ca39ca56ef0ea439d71fa90d8f208
Nikola Šantić <nikola@readcubes-MacBook-Pro.local>
2017-09-11 23:12:17 +0200

    Added comments for crawlers, minor code improvements

--- app/helpers/graetzls_helper.rb
+++ app/helpers/graetzls_helper.rb
@@ -2,8 +2,4 @@ module GraetzlsHelper
   def filters_for(user, graetzl)
     render "graetzls/#{user ? 'users' : 'guests'}/filters", graetzl: graetzl
   end
-
-  def graetzl_stream_for(user)
-    render "graetzls/#{user ? 'users' : 'guests'}/stream"
-  end
 end
