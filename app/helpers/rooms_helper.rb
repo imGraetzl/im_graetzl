f5c29667647df884919831ac6e20f60cc26a6175
Michael Walchhütter <michael.walchhuetter@gmail.com>
2017-11-01 17:25:54 +0100

    raumteiler stylings - Vienna Overview Pages and Raumteiler Detail-Page

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -2,10 +2,10 @@ module RoomsHelper
 
   def filter_room_types
     [
-      ['Allen Raumteiler', '', 'data-label' => 'Allen Raumteiler'],
+      ['Alle Raumteiler', '', 'data-label' => 'Alle Raumteiler'],
       ['Alle Räume', 'offer', 'data-label' => 'Alle Räume'],
       ['Räume zum Einmieten', 'offer-0', 'data-label' => 'Räume zum Einmieten'],
-      ['Räume zum Neuanmieten', 'offer-1', 'data-label' => 'Räume zum Neuanmieten'],
+      ['Räume zur Neuanmietung', 'offer-1', 'data-label' => 'Räume zur Neuanmietung'],
       ['Alle Raumsuchende', 'demand', 'data-label' => 'Alle Raumsuchende'],
       ['Raumsuchende zum Einmieten', 'demand-0', 'data-label' => 'Raumsuchende zum Einmieten'],
       ['Raumsuchende zur gemeinsamen Neuanmietung', 'demand-1', 'data-label' => 'Raumsuchende zur gemeinsamen Neuanmietung'],
