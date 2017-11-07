9c1c0034f3cf46b37b1de7e6c6a5bf251b14d8ea
Dražen Lučanin <kermit666@gmail.com>
2017-11-07 15:47:11 +0100

    add bubble navigation to the Bezirk level

--- app/assets/javascripts/controllers/districts.js
+++ app/assets/javascripts/controllers/districts.js
@@ -4,6 +4,7 @@ APP.controllers.districts = (function() {
     initMenu();
     initMap();
     initFilter();
+    initMobileNav();
   }
 
   function initMenu() {
@@ -43,6 +44,28 @@ APP.controllers.districts = (function() {
     });
 
     $('.autosubmit-filter').submit();
+  }
+
+  function initMobileNav() {
+    var $dropdown = $(".filter-stream .input-select select");
+    $(".filter-stream .iconfilter").not('.createentry, .loginlink').each(function() {
+        var $this = $(this),
+            link = $this.prop('href'),
+            txt = $this.find('.txt').text();
+
+        $dropdown.append(getOption());
+        $dropdown.on('change', function() {
+            document.location.href = $dropdown.val();
+        });
+
+        function getOption() {
+            if($this.hasClass('active'))
+                return '<option selected value="'+ link +'">'+ txt +'</option>';
+            return '<option value="'+ link +'">'+ txt +'</option>';
+        }
+
+    });
+    $('[data-behavior=createTrigger]').jqDropdown('attach', '[data-behavior=createContainer]');
   }
 
   return {
