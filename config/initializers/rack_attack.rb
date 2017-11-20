ecec03ceb0fd2d54282e70a1177e7a6e5057dde4
Nikola Šantić <nikola@readcubes-MacBook-Pro.local>
2017-11-20 12:52:48 +0100

    Block wp-login.php requests

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,7 +1,8 @@
 class Rack::Attack
 
   blocklist('block script kiddies') do |req|
-    req.post? && req.path =~ /\.(php|asp|aspx|jsp|cgi)$/i
+    (req.post? && req.path =~ /\.(php|asp|aspx|jsp|cgi)$/i) ||
+    (req.get? && req.path =~ /wp-login\.php$/)
   end
 
 end
