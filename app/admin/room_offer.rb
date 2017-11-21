d5c198785d5926f147c5e9ad8ca87b334bf89d22
Dražen Lučanin <kermit666@gmail.com>
2017-11-21 13:31:56 +0100

    custom raumteiler admin form w/ sorting

--- app/admin/room_offer.rb
+++ app/admin/room_offer.rb
@@ -2,6 +2,7 @@ ActiveAdmin.register RoomOffer do
   include ViewInApp
   menu parent: 'Rooms'
   includes :graetzl, :location
+  actions :all, except: [:new, :create]
 
   scope :all, default: true
 
@@ -14,9 +15,11 @@ ActiveAdmin.register RoomOffer do
   filter :created_at
   filter :updated_at
 
+  form partial: 'form'
+
   permit_params :user_id, :slogan, :graetzl_id, :district_id, :location_id, :room_description, :total_area,
     :rented_area, :daily_rent, :longterm_rent, :owner_description, :tenant_description, :wants_collaboration,
-    :slug, :offer_type,
+    :slug, :offer_type, :avatar, :first_name, :last_name, :website, :email, :phone, :location_files,
     address_attributes: [ :id, :_destroy, :street_name, :street_number, :zip, :city, :coordinates, :description]
 
 
