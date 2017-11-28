9a1872c29eb4845909c4b4b36851e5671680a729
Nikola Šantić <nikola@readcubes-MacBook-Pro.local>
2017-11-28 12:52:07 +0100

    Updated admin

--- spec/factories/room_demands.rb
+++ spec/factories/room_demands.rb
@@ -2,8 +2,6 @@ FactoryGirl.define do
   factory :room_demand do
     slogan "MyString"
     needed_area "9.99"
-    daily_rent false
-    longterm_rent false
     demand_description "MyText"
     personal_description "MyText"
     wants_collaboration false
