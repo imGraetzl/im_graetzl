58d81ec2bf717af50bd3c021aa09c8948f861e74
Michael Walchhütter <michael.walchhuetter@gmail.com>
2017-12-21 22:10:25 +0100

    Custom User friendly Error Pages
    https://mattbrictson.com/dynamic-rails-error-pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
+
   ActiveAdmin.routes(self)
 
   devise_scope :user do
