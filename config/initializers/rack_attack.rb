c935abd0c30a581f2c12ef58ff93ed85ac26cbe9
Nikola Šantić <nikola@readcubes-MacBook-Pro.local>
2018-02-04 18:26:21 +0100

    Block sniffers, don't report routing errors

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,8 +1,7 @@
 class Rack::Attack
 
   blocklist('block script kiddies') do |req|
-    (req.post? && req.path =~ /\.(php|asp|aspx|jsp|cgi)$/i) ||
-    (req.get? && req.path =~ /wp-login\.php$/)
+    (req.path =~ /\.(php|asp|aspx|jsp|cgi)$/i)
   end
 
 end
