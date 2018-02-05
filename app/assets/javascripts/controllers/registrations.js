6241bc44ab5e963fd74e2ba1c9ebdabc9dfcdc1c
Michael Walchhütter <michael.walchhuetter@gmail.com>
2018-02-05 22:34:32 +0100

    set registration graetzl in local storage for analytics tracking

--- app/assets/javascripts/controllers/registrations.js
+++ app/assets/javascripts/controllers/registrations.js
@@ -5,6 +5,16 @@ APP.controllers.registrations = (function() {
         APP.components.inputTextareaMovingLabel();
         APP.components.addressSearchAutocomplete();
         APP.components.graetzlSelect();
+
+        // Set Registration Graetzl In Local Storage (for GA Event Tracking)
+        $(document).ready(function() {
+          $('#btn-register').on('click', function() {
+            var graetzl = $('#graetzl').html();
+            graetzl = graetzl.replace('&amp;','&');
+            localStorage.setItem('Graetzl', graetzl);
+          });
+        });
+
     }
 
 
--- app/assets/javascripts/controllers/sessions.js
+++ app/assets/javascripts/controllers/sessions.js
@@ -4,6 +4,22 @@ APP.controllers.sessions = (function() {
 
         APP.components.inputTextareaMovingLabel();
 
+        $(document).ready(function() {
+          // Set FB and Analytics Tracking for new registered Users
+          var newRegConfUser = $("#flash .alert");
+          if (newRegConfUser.exists()) {
+            if( newRegConfUser.html().indexOf('Dein Account ist nun freigeschaltet') >= 0){
+              var graetzl = localStorage.getItem('Graetzl');
+              // FB
+              fbq('track', 'CompleteRegistration');
+              // Analytics
+              gtag('event', 'Registration', {
+                'event_category': graetzl
+              });
+            }
+          }
+        });
+
     }
 
 
