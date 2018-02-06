5569d95b918036aa07d1cf72b67aa0859917d5b6
Michael Walchhütter <michael.walchhuetter@gmail.com>
2018-02-06 21:56:03 +0100

    bug fix

--- app/jobs/mailchimp_room_online_job.rb
+++ app/jobs/mailchimp_room_online_job.rb
@@ -3,25 +3,24 @@ class MailchimpRoomOnlineJob < ApplicationJob
   def perform(room)
     list_id = Rails.application.secrets.mailchimp_list_id
 
-    room.users.each do |user|
-      member_id = mailchimp_member_id(user)
+    member_id = mailchimp_member_id(room.user)
 
-      begin
-        g = Gibbon::Request.new
-        g.timeout = 30
-        g.lists(list_id).members(member_id).update(body: {
-          merge_fields: {
-            ROOM_TITLE: room.slogan
-          }
-        })
-      rescue Gibbon::MailChimpError => mce
-        SuckerPunch.logger.error("subscribe failed: due to #{mce.message}")
-        raise mce
-      rescue => e
-        SuckerPunch.logger.error("subscribe failed: due to #{e.message}")
-        raise e
-      end
+    begin
+      g = Gibbon::Request.new
+      g.timeout = 30
+      g.lists(list_id).members(member_id).update(body: {
+        merge_fields: {
+          ROOM_TITLE: room.slogan
+        }
+      })
+    rescue Gibbon::MailChimpError => mce
+      SuckerPunch.logger.error("subscribe failed: due to #{mce.message}")
+      raise mce
+    rescue => e
+      SuckerPunch.logger.error("subscribe failed: due to #{e.message}")
+      raise e
     end
+
   end
 
   def mailchimp_member_id(user)
