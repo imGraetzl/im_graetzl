09671b726201628bbb252ca7d1689e8233c1e233
Nikola Šantić <nikola@readcubes-MacBook-Pro.local>
2018-04-24 13:55:34 +0200

    Improved rooms index db queries. Increased notification ping interval

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
   # hide staging app from public
   http_basic_authenticate_with name: 'user', password: 'secret' if Rails.env.staging? && !(ENV["ALLOW_WORKER"] == 'true')
 
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,7 +7,7 @@ class RoomsController < ApplicationController
     room_offers = filter_offers(room_offers)
     room_offers = room_offers.by_currentness.page(params[:page]).per(15)
 
-    room_demands = room_demands_scope.includes(:user, :graetzls, :districts)
+    room_demands = room_demands_scope.includes(:user, :graetzls, :districts, :room_categories)
     room_demands = filter_demands(room_demands)
     room_demands = room_demands.by_currentness.page(params[:page]).per(15)
 
@@ -28,7 +28,7 @@ class RoomsController < ApplicationController
   def room_demands_scope
     if params[:district_id].present?
       district = District.find(params[:district_id])
-      RoomDemand.includes(:graetzls).where(graetzls: {id: district.graetzl_ids})
+      RoomDemand.joins(:room_demand_graetzls).where(room_demand_graetzls: {graetzl_id: district.graetzl_ids}).distinct
     else
       RoomDemand.all
     end
@@ -45,9 +45,9 @@ class RoomsController < ApplicationController
       end
     end
 
-    room_category_ids = params.dig(:filter, :room_category_ids)
-    if room_category_ids.present? && room_category_ids.any?(&:present?)
-      offers = offers.includes(:room_categories).where(room_categories: {id: room_category_ids})
+    room_category_ids = params.dig(:filter, :room_category_ids)&.select(&:present?)
+    if room_category_ids.present?
+      offers = offers.joins(:room_offer_categories).where(room_offer_categories: {room_category_id: room_category_ids}).distinct
     end
 
     graetzl_ids = params.dig(:filter, :graetzl_ids)
@@ -69,14 +69,14 @@ class RoomsController < ApplicationController
       end
     end
 
-    room_category_ids = params.dig(:filter, :room_category_ids)
-    if room_category_ids.present? && room_category_ids.any?(&:present?)
-      demands = demands.includes(:room_categories).where(room_categories: {id: room_category_ids})
+    room_category_ids = params.dig(:filter, :room_category_ids)&.select(&:present?)
+    if room_category_ids.present?
+      demands = demands.joins(:room_demand_categories).where(room_demand_categories: {room_category_id: room_category_ids}).distinct
     end
 
-    graetzl_ids = params.dig(:filter, :graetzl_ids)
-    if graetzl_ids.present? && graetzl_ids.any?(&:present?)
-      demands = demands.includes(:graetzls).where(graetzls: {id: graetzl_ids})
+    graetzl_ids = params.dig(:filter, :graetzl_ids)&.select(&:present?)
+    if graetzl_ids.present?
+      demands = demands.joins(:room_demand_graetzls).where(room_demand_graetzls: {graetzl_id: graetzl_ids}).distinct
     end
 
     demands
