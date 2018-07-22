b40e159295ca21bd394eb528ef31ff9370691fcc
Nikola Šantić <nikola@readcubes-MacBook-Pro.local>
2018-07-22 18:38:41 +0200

    Fixed group_mailer

--- app/mailers/group_mailer.rb
+++ app/mailers/group_mailer.rb
@@ -7,14 +7,14 @@ class GroupMailer
     MandrillMailer.deliver(template: 'group-join-request-new', message: {
       to: group_admins.map{ |u| { email: u.email } },
       global_merge_vars: [
-        { name: 'owner_first_name', group_admins.map{ |u| { content: u.first_name } } },
         { name: 'username', content: join_request.user.username },
         { name: 'first_name', content: join_request.user.first_name },
         { name: 'last_name', content: join_request.user.last_name },
         { name: 'e_mail', content: join_request.user.email },
         { name: 'user_type', content: join_request.user.business? ? 'business' : 'normal' },
         { name: 'request_message', content: join_request.request_message },
-      ]
+      ],
+      merge_vars: group_admins.map { |user| owner_personal_vars(user) }
     })
   end
 
@@ -29,6 +29,15 @@ class GroupMailer
         { name: 'group_name', content: group.title },
       ]
     })
+  end
+
+  private
+
+  def owner_personal_vars(user)
+    {
+      rcpt: user.email,
+      vars: [{ name: 'owner_first_name', content: user.first_name }]
+    }
   end
 
 end
