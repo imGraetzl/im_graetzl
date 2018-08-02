fb2ae47c305d06618122a2a9863d04a4a5ac572a
malano <michael.walchhuetter@gmail.com>
2018-08-02 17:51:16 +0200

    try bugfix

--- app/models/notifications/new_group_user.rb
+++ app/models/notifications/new_group_user.rb
@@ -14,7 +14,7 @@ class Notifications::NewGroupUser < Notification
   def mail_vars
     {
       group_name: activity.trackable.group.title,
-      group_url: group_url (activity.trackable.group, DEFAULT_URL_OPTIONS),
+      group_url: group_url(activity.trackable.group, DEFAULT_URL_OPTIONS),
       group_user_first_name: activity.trackable.user.name,
       group_user_last_name: activity.trackable.user.last_name,
       group_user_avatar_url: Notifications::AvatarService.new(activity.trackable.user).call,
