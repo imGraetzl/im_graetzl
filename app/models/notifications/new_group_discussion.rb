0fddab016cef21374ce6ba4b9d0059ce994a4a36
malano <michael.walchhuetter@gmail.com>
2018-08-02 14:07:28 +0200

    added mailvars

--- app/models/notifications/new_group_discussion.rb
+++ app/models/notifications/new_group_discussion.rb
@@ -15,9 +15,10 @@ class Notifications::NewGroupDiscussion < Notification
     {
       group_name: activity.trackable.group.title,
       discussion_title: activity.trackable.title,
+      first_post_content: activity.trackable.discussion_posts.first.content.truncate(300, separator: ' '),
       discussion_url: group_discussion_path(activity.trackable.group, activity.trackable),
       owner_firstname: activity.owner.first_name,
-      owner_avatar_url: Notifications::AvatarService.new(activity.trackable.user).call,
+      owner_avatar_url: Notifications::AvatarService.new(activity.trackable.user).call
     }
   end
 
