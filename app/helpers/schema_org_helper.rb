59a3394cf86ca88d2eb524f5e3f6b10a01ed9ee5
malano <michael.walchhuetter@gmail.com>
2018-09-21 13:03:00 +0200

    added microdata - location and person for meetings

--- app/helpers/schema_org_helper.rb
+++ app/helpers/schema_org_helper.rb
@@ -6,13 +6,6 @@ module SchemaOrgHelper
     content_tag(:script, final_hash.to_json, {type: 'application/ld+json'}, false) # false is used here to prevent html character escaping
   end
 
-  # Create Structured Data for Locations
-  def structured_data_location (location)
-    hash = {:@type => 'Place'}
-    hash[:name] = location.name
-    return hash
-  end
-
   # Create Structured Data for Meetings
   def structured_data_meeting (meeting)
     hash = {:@type => 'Event'}
@@ -21,9 +14,53 @@ module SchemaOrgHelper
     hash[:startDate] = I18n.localize(meeting.starts_at_date, format:'%Y-%m-%d') if meeting.starts_at_date
     hash[:image] = attachment_url(meeting, :cover_photo, host: request.url, fallback: 'meta/og_logo.png')
     hash[:url] = meeting_url(meeting)
-    if meeting.location
-      hash[:location] = structured_data_location(meeting.location)
+
+    hash[:location] = {:@type => 'Place'} # Object for Event Location or Address
+    if !meeting.address.nil? # Take Adress from Meeting if exists
+      hash[:location][:address] = structured_data_address(meeting.address)
+      hash[:location][:name] = meeting.address.description if meeting.address.description.present?
     end
+
+    if meeting.location # If Location exists
+      hash[:location][:name] = meeting.location.name
+      hash[:location][:image] = attachment_url(meeting.location, :cover_photo, host: request.url, fallback: 'meta/og_logo.png')
+      hash[:location][:sameAs] = location_url(meeting.location)
+      # Take Address from Location if no Meeting Address exists
+      if meeting.address.nil? && meeting.location.address
+        hash[:location][:address] = structured_data_address(meeting.location.address)
+      end
+    end
+
+    if !meeting.initiator.nil? # Creator of Meeting
+      hash[:organizer] = structured_data_person(meeting.initiator)
+    end
+
+    return hash
+  end
+
+  # Create Structured Data for Locations
+  def structured_data_address (address)
+    hash = {:@type => 'PostalAddress'}
+    hash[:streetAddress] = address.street if address.street
+    hash[:addressLocality] = address.city if address.city
+    hash[:addressRegion] = address.city if address.city
+    hash[:postalCode] = address.zip if address.zip
+    hash[:addressCountry] = 'AT'
+    return hash
+  end
+
+  # Create Structured Data for Persons
+  def structured_data_person (user)
+    hash = {:@type => 'Person'}
+    hash[:name] = user.full_name
+    hash[:image] = attachment_url(user, :avatar, host: request.url, fallback: 'meta/og_logo.png')
+    return hash
+  end
+
+  # Create Structured Data for Locations
+  def structured_data_location (location)
+    hash = {:@type => 'Place'}
+    hash[:name] = location.name
     return hash
   end
 
