f91b4fb1a82f2c680d019a24e8fdfe0d23a74903
malano <michael.walchhuetter@gmail.com>
2018-09-28 14:22:32 +0200

    finetuning filter and query results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,20 +1,25 @@
 class SearchController < ApplicationController
   def index
     if params[:q]
+
       searchterm = params[:q]
       page = params[:page] || 1
+      label = ''
+      sort = ''
 
-      if params[:label]
-        if params[:label] == 'all'
-          label = ''
-        else
-          label = " + ' more:' + " + params[:label]
-        end
-      else
-        label = ''
+      if (params[:label] && params[:label] != 'all')
+        label = " more:pagemap:document-type:" + params[:label]
       end
 
-      @results = GoogleCustomSearchApi.search(searchterm + label, page: page, as_sitesearch: params[:as_sitesearch])
+      if (params[:label] && params[:label] == 'rooms')
+        label = " more:pagemap:document-type:room_offer OR more:pagemap:document-type:room_demand"
+      end
+
+      if params[:label] == 'meeting'
+        #sort = 'date:startDate:d'
+      end
+
+      @results = GoogleCustomSearchApi.search(searchterm + label, sort: sort, page: page, num:9, as_sitesearch: params[:as_sitesearch])
 
     end
   end
