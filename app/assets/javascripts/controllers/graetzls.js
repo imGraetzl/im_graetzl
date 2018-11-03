696d9c915b546713318858ab5114d0f6aa7a388c
malano <michael.walchhuetter@gmail.com>
2018-11-03 21:10:07 +0100

    category flag added

--- app/assets/javascripts/controllers/graetzls.js
+++ app/assets/javascripts/controllers/graetzls.js
@@ -14,43 +14,27 @@ APP.controllers.graetzls = (function() {
             autoWidth: false,
             slideMove: 3, // slidemove will be 1 if loop is true
             slideMargin: 15,
-
             addClass: '',
             mode: "slide",
             useCSS: true,
             cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
             easing: 'linear', //'for jquery animation',////
-
             speed: 400, //ms'
             auto: false,
-            loop: false,
+            loop: true,
             slideEndAnimation: true,
             pause: 2000,
-
             keyPress: false,
             controls: true,
             prevHtml: '',
             nextHtml: '',
-
-            //rtl:false,
             adaptiveHeight:false,
-
-            //vertical:false,
-            //verticalHeight:500,
-            //vThumbWidth:100,
-
-            //thumbItem:10,
             pager: false,
-            //gallery: false,
-            //galleryMargin: 5,
-            //thumbMargin: 5,
             //currentPagerPosition: 'middle',
-
             enableTouch:true,
-            enableDrag:true,
+            enableDrag:false,
             freeMove:true,
             swipeThreshold: 40,
-
             responsive : [
               {
                   breakpoint:850,
