9bd9d87db1a6970c80a0aa4b21e4ca7988098424
Nikola Šantić <nikola@readcubes-MacBook-Pro.local>
2019-01-25 16:41:58 +0100

    Business users must have category and interests selected when registering

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,6 +44,9 @@ class User < ApplicationRecord
   validates :terms_and_conditions, acceptance: true
   validates :website, url: true, allow_blank: true
 
+  validates :location_category_id, presence: true, on: :create, if: :business?
+  validates :business_interests, presence: true, on: :create, if: :business?
+
   before_validation { self.username.squish! if self.username }
 
   after_update :update_mailchimp, if: -> { email_changed? || first_name_changed? || last_name_changed? || newsletter_changed? }
