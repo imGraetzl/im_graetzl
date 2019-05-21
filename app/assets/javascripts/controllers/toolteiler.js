ab0cb5aee1e3e9c79523e18887e7fd5c61fca759
malano <michael.walchhuetter@gmail.com>
2019-05-21 22:52:57 +0200

    layout finetuning

--- app/assets/javascripts/controllers/toolteiler.js
+++ app/assets/javascripts/controllers/toolteiler.js
@@ -7,11 +7,29 @@ APP.controllers.toolteiler = (function() {
     }
 
     function initToolteilerDetail() {
-      $('.datepicker').pickadate({
+
+      $('.starts_at_date').pickadate({
         formatSubmit: 'yyyy-mm-dd',
         hiddenName: true,
         format: 'ddd, dd mmm, yyyy',
+        onSet: function(context) {
+          if (typeof context.select !== "undefined") {
+            var d = moment(context.select).locale('de').format('l');
+            console.log(d);
+          }
+
+        }
       });
+
+      $('.ends_at_date').pickadate({
+        formatSubmit: 'yyyy-mm-dd',
+        hiddenName: true,
+        format: 'ddd, dd mmm, yyyy',
+        //disable: [
+          //{ from: -365, to: request_start_date }
+        //]
+      });
+      
     }
 
     return {
