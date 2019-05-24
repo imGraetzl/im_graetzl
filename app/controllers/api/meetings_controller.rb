d416931f1ad0d4660f6ca082ec21fd3add54aa8e
malano <michael.walchhuetter@gmail.com>
2019-05-24 11:12:28 +0200

    bugfix - min_created_at param

--- app/controllers/api/meetings_controller.rb
+++ app/controllers/api/meetings_controller.rb
@@ -3,14 +3,14 @@ class Api::MeetingsController < Api::ApiController
   def index
     @meetings = Meeting.active.visible_to_all.where(approved_for_api: true)
     from_date, to_date, min_created_at = get_date_range
-    if from_date || to_date || min_created_at
+    if from_date || to_date
       @meetings = @meetings.where("starts_at_date >= ?", from_date) if from_date
       @meetings = @meetings.where("starts_at_date <= ?", to_date) if to_date
-      @meetings = @meetings.where("updated_at > ?", min_created_at) if min_created_at
     else
       @meetings = @meetings.upcoming
     end
 
+    @meetings = @meetings.where("updated_at > ?", min_created_at) if min_created_at
     @meetings = @meetings.includes(:graetzl, :address, location: [:address, :graetzl])
 
     render json: MeetingsSerializer.new(@meetings, request)
