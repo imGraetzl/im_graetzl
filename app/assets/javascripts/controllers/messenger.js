95c32d9c52e082c4cefa3f53e0444a73cd55260e
malano <michael.walchhuetter@gmail.com>
2019-06-23 22:14:58 +0200

    finetuning

--- app/assets/javascripts/controllers/messenger.js
+++ app/assets/javascripts/controllers/messenger.js
@@ -15,19 +15,21 @@ APP.controllers.messenger = (function() {
     $(".chat-panel").bind('scroll', function(e) {
 
       var elem = $(e.currentTarget);
-      var insert_content = $('.chat-bubble:nth-child(-n+4)'); // Fake Clone Content
+      var insert_content = $('.chat-bubble:nth-child(-n+8)'); // Fake Clone Content
 
       if (elem.scrollTop() == 0 && loading_bubbles == false) {
         loading_bubbles = true;
         //console.log("top rechaed"); // Check for older Messages and load them
         $('.loading-spinner').clone().insertBefore('.chat-bubble:first').hide().fadeIn('200', function(){
           setTimeout(function() { // Animate Time for Ajax
+            var loading_spinner_height = $('.chat-panel .loading-spinner').outerHeight(true);
+            console.log(loading_spinner_height);
             $('.chat-panel .loading-spinner').remove();
             // Demo for loading more content ...
             insert_content.clone().insertBefore('.chat-bubble:first').hide().fadeIn();
 
-            // Move to correct position, so that content is at same posotion after inserting. check on different screen sizes !!!! evtl anders lösen?!
-            $(".chat-panel").scrollTop(insert_content.outerHeight() * 4); // 4 Bubbles inserted Fake Content
+            // Move to correct position, so that content is at same posotion after inserting. check on different screen sizes !!!! Mobile Problems ..?!
+            $(".chat-panel").scrollTop(insert_content.outerHeight(true) * 8 - loading_spinner_height); // 4 Bubbles inserted Fake Content
             loading_bubbles = false; // reset loading to default
           }, 2000);
         });
