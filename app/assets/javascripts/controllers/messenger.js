0263c28bb28dd12ea7271804f2cb9437e6ec74dc
malano <michael.walchhuetter@gmail.com>
2019-06-27 14:14:13 +0200

    single user options added

--- app/assets/javascripts/controllers/messenger.js
+++ app/assets/javascripts/controllers/messenger.js
@@ -7,13 +7,15 @@ APP.controllers.messenger = (function() {
 
   function initMessenger() {
 
+    var msg = '1';
+
     // Jump to end of Chat Messages -> Show newest.
     $(".chat-panel").scrollTop($(".chat-panel")[0].scrollHeight);
 
     // When scrolling to top of Chat Messages -> Load older before
     var loading_bubbles = false;
     var numberMessages = 5;
-    var insert_fake_content = $('.chatMsg:nth-child(-n+'+numberMessages+')'); // Fake Clone Content
+    var insert_fake_content = $('.fake'+msg+' .chatMsg:nth-child(-n+'+numberMessages+')'); // Fake Clone Content
     var loadedBubbleHeight = 0;
     var loading_spinner_height = 0;
 
@@ -66,17 +68,17 @@ APP.controllers.messenger = (function() {
       $(".chat-message").animate({'height':'38px'}, 100, function() {
         var chatMessage = $('.chat-message').val();
         $('.chat-message').val('');
-        var bContainer = $('<div class="chatMsg -left"><img class="attachment user avatar img-round" src="https://d1dcf21ighh4hq.cloudfront.net/attachments/fad5d1e8df9b7eba405c9f2deec155f27c709f09/store/fill/400/400/0a90d925fcde3d6fa5b1b3f35f7ba2753972a5c6164acc222ab2e172d1ae/avatar.png"><div class="bubble">'+chatMessage+'</div></div>');
-        bContainer.insertAfter('.chat-panel .chatMsg:last');
+        var bContainer = $('<div class="chatMsg -right"><img class="attachment user avatar img-round" src="https://d1dcf21ighh4hq.cloudfront.net/attachments/fad5d1e8df9b7eba405c9f2deec155f27c709f09/store/fill/400/400/0a90d925fcde3d6fa5b1b3f35f7ba2753972a5c6164acc222ab2e172d1ae/avatar.png"><div class="bubble">'+chatMessage+'</div></div>');
+        bContainer.insertAfter('.chat-panel .fake'+msg+' .chatMsg:last');
         $(".chat-panel").scrollTop($(".chat-panel")[0].scrollHeight);
       });
     });
 
     // Fake Switch Message
-    $('div[data-msg]').click(function() {
+    $('div[data-msg] .wrp').click(function() {
       $(".single-user").removeClass('-active');
-      $(this).addClass('-active');
-      var msg = $(this).attr("data-msg");
+      $(this).closest(".single-user").addClass('-active');
+      msg = $(this).closest(".single-user").attr("data-msg");
       $(".message-wrapper").hide();
       $(".message-wrapper.fake"+msg).fadeIn();
       $(".chat-panel").scrollTop($(".chat-panel")[0].scrollHeight);
