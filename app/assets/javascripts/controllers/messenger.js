c88ffe011a8e49d6dfbd481304a49ae5f05583c3
malano <michael.walchhuetter@gmail.com>
2019-06-28 11:47:53 +0200

    scrollfix

--- app/assets/javascripts/controllers/messenger.js
+++ app/assets/javascripts/controllers/messenger.js
@@ -9,7 +9,7 @@ APP.controllers.messenger = (function() {
 
     var msg = '1';
     unscroll();
-    document.addEventListener('touchmove', function() { e.preventDefault(); }, { passive:false });
+    document.addEventListener('touchstart', function() { e.preventDefault(); }, { passive:false });
 
     // Jump to end of Chat Messages -> Show newest.
     $(".chat-panel").scrollTop($(".chat-panel")[0].scrollHeight);
