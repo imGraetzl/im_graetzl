191edfe27f0ab836c54137c88582da6c1f73b6f2
malano <michael.walchhuetter@gmail.com>
2019-09-10 21:32:01 +0200

    mailings

--- app/mailers/tool_offer_mailer.rb
+++ app/mailers/tool_offer_mailer.rb
@@ -11,19 +11,19 @@ class ToolOfferMailer < ApplicationMailer
   def rental_approved(tool_rental)
     @tool_rental = tool_rental
     headers("X-MC-Tags" => "tool-rental-approved")
-    mail(to: @tool_rental.user.email, subject: "Your Rental has been approved")
+    mail(to: @tool_rental.user.email, subject: "Deine Toolteiler Buchung wurde bestätigt")
   end
 
   def rental_rejected(tool_rental)
     @tool_rental = tool_rental
     headers("X-MC-Tags" => "tool-rental-rejected")
-    mail(to: @tool_rental.user.email, subject: "Your Rental has been rejected")
+    mail(to: @tool_rental.user.email, subject: "Deine Toolteiler Anfrage wurde leider abgelehnt.")
   end
 
   def rental_canceled(tool_rental)
     @tool_rental = tool_rental
     headers("X-MC-Tags" => "tool-rental-canceled")
-    mail(to: @tool_rental.tool_offer.user.email, subject: "Tool Rental canceled")
+    mail(to: @tool_rental.tool_offer.user.email, subject: "#{@tool_rental.user.first_name} hat die Toolteiler Anfrage zurückgezogen")
   end
 
 end
