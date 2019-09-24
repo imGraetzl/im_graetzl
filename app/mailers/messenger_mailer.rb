65475bcb300bb7e06a2677836a3a9355cd63db27
Nikola Šantić <anrakee@gmail.com>
2019-09-25 00:35:54 +0200

    Added thread url

--- app/mailers/messenger_mailer.rb
+++ app/mailers/messenger_mailer.rb
@@ -3,6 +3,7 @@ class MessengerMailer < ApplicationMailer
   def unseen_messages(user, messages)
     @user = user
     @messages = messages
+    @thread = @messages.first.user_message_thread
     mail(to: user.email, subject: "Du hast neue Nachrichten im Messenger.")
   end
 
