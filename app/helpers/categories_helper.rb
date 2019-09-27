ef669a2ccc192e36547a682174201d731edb9961
malano <michael.walchhuetter@gmail.com>
2019-09-27 20:42:58 +0200

    bugfix location icon & user verlinkungen

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,6 +1,6 @@
 module CategoriesHelper
   def location_category_icon(category)
-    icon = category.icon || 'icon-location'
-    content_tag(:svg, content_tag(:use, nil, { 'xlink:href' => "##{icon}" }), class: icon)
+    icon = category.icon || 'location'
+    icon_tag(icon)
   end
 end
