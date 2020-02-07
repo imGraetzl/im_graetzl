05a5bd07ba2b30a5a6e9d6763325168b6bb7af13
malano <michael.walchhuetter@gmail.com>
2020-02-07 23:28:31 +0100

    bbcode adaptions

--- config/initializers/bb-ruby.rb
+++ config/initializers/bb-ruby.rb
@@ -1,10 +1,42 @@
 module BBRuby
   if @@tags
     @@tags['h2'] = [
+      /\[h2\](.*?)\[\/h2\1?\]/mi,
+      '<h2 class="bb">\1</h2>',
+      'h2',
+      '[h2]h2[/h2]',
+      :h2]
+  end
+  if @@tags
+    @@tags['h3'] = [
       /\[h3\](.*?)\[\/h3\1?\]/mi,
-      '<h3>\1</h3>',
+      '<h3 class="bb">\1</h3>',
       'h3',
       '[h3]h3[/h3]',
+      :h3]
+  end
+  if @@tags
+    @@tags['gallery'] = [
+      /\[gallery\](.*?)\[\/gallery\1?\]/mi,
+      '<div class="entryImgUploads">\1</div>',
+      'gallery',
+      '[gallery]gallery[/gallery]',
+      :h3]
+  end
+  if @@tags
+    @@tags['imgtxt'] = [
+      /\[imgtxt\](.*?)\[\/imgtxt\1?\]/mi,
+      '<div class="imgtxt">\1</div>',
+      'imgtxt',
+      '[imgtxt]imgtxt[/imgtxt]',
+      :h3]
+  end
+  if @@tags
+    @@tags['txt'] = [
+      /\[txt\](.*?)\[\/txt\1?\]/mi,
+      '<span>\1</span>',
+      'txt',
+      '[txt]txt[/txt]',
       :h3]
   end
 end
