60b8d7e680eb25d6fab5ea349046901519632fc0
malano <michael.walchhuetter@gmail.com>
2020-03-12 11:40:23 +0100

    delete going_to date if meeting_additional_date is deleted

--- app/models/meeting_additional_date.rb
+++ app/models/meeting_additional_date.rb
@@ -3,6 +3,8 @@ class MeetingAdditionalDate < ApplicationRecord
   has_many :going_tos
   has_many :users, through: :going_tos
 
+  before_destroy :check_for_going_tos, prepend: true
+
   def display_starts_at_date
     if starts_at_time
       "#{I18n.localize(starts_at_date, format:'%a, %d. %B %Y')}, #{I18n.localize(starts_at_time, format:'%H:%M')} Uhr"
@@ -10,5 +12,14 @@ class MeetingAdditionalDate < ApplicationRecord
       "#{I18n.localize(starts_at_date, format:'%a, %d. %B %Y')}"
     end
   end
+
+  private
+
+    def check_for_going_tos
+      self.going_tos.update_all(
+        going_to_date: nil,
+        going_to_time: nil
+      )
+    end
 
 end
