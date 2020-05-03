2d7198d45b132a577c03babce8a1e31b27213edc
malano <michael.walchhuetter@gmail.com>
2020-05-03 21:14:19 +0200

    cross_platform comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,13 @@ class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.new(comment_params)
     if @comment.save
-      @comment.commentable.create_activity :comment, owner: current_user, recipient: @comment
+
+      if @comment.commentable_type == 'Meeting' && @comment.commentable.online_meeting
+        @comment.commentable.create_activity :comment, owner: current_user, recipient: @comment, parameters: { cross_platform: true }
+      else
+        @comment.commentable.create_activity :comment, owner: current_user, recipient: @comment
+      end
+
     end
   end
 
