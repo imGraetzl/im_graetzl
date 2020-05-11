66133237d0fc2c1148dcd4d261a5a035bae8c3b4
malano <michael.walchhuetter@gmail.com>
2020-05-11 21:19:29 +0200

    wording

--- app/models/meeting_category.rb
+++ app/models/meeting_category.rb
@@ -1,14 +1,6 @@
 class MeetingCategory < ApplicationRecord
   has_many :meetings
 
-  def category_width_date_range_html
-    if starts_at_date && ends_at_date
-      "#{title}<span class='date'> – #{I18n.localize(starts_at_date, format:'%d. %B')} bis #{I18n.localize(ends_at_date, format:'%d. %B')}</span>"
-    else
-      title
-    end
-  end
-
   def category_width_date_range
     if starts_at_date && ends_at_date
       "#{title} – #{I18n.localize(starts_at_date, format:'%d. %B')} bis #{I18n.localize(ends_at_date, format:'%d. %B')}"
