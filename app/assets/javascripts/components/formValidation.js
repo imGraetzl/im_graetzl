b4cb615ded8b5fdc43952f331597095f3870ce7d
malano <michael.walchhuetter@gmail.com>
2020-09-18 14:04:51 +0200

    refactored validation

--- app/assets/javascripts/components/formValidation.js
+++ app/assets/javascripts/components/formValidation.js
@@ -8,15 +8,15 @@ APP.components.formValidation = (function() {
     $(".form-block :input.-required").on("blur", function() {
       var $error_msg = $( "<span class='error_msg required_msg'> *</span>" );
       var $label = $("label[for='"+this.id+"']");
-      if (!$(this).val()) {
+      if ($(this).val()) {
+        $(this).removeClass('-invalid');
+        $label.removeClass('-invalid');
+        $label.find('span.error_msg').remove();
+      } else {
         $(this).addClass('-invalid');
         $label.addClass('-invalid');
         $label.find('span.error_msg').remove();
         $label.find('span').append($error_msg);
-      } else {
-        $(this).removeClass('-invalid');
-        $label.removeClass('-invalid');
-        $label.find('span.error_msg').remove();
       }
     });
 
@@ -36,22 +36,22 @@ APP.components.formValidation = (function() {
     $(".form-block :input.-validate").on("blur", function() {
       var $error_msg = $( "<span class='error_msg'> (Fehler)</span>" );
       var $label = $("label[for='"+this.id+"']");
-      if ($(this).val() && !this.checkValidity()) {
+      if (this.checkValidity()) {
+        $(this).removeClass('-invalid');
+        $label.removeClass('-invalid');
+        $label.find('span.error_msg').remove();
+      } else {
         $(this).addClass('-invalid');
         $label.addClass('-invalid');
         $label.find('span.error_msg').remove();
         $label.find('span').append($error_msg);
-      } else if ($(this).val() && this.checkValidity()) {
-        $(this).removeClass('-invalid');
-        $label.removeClass('-invalid');
-        $label.find('span.error_msg').remove();
       }
     });
 
     // Type in invalid fields
     $(document).on("keyup", ':input.-validate.-invalid', function(e) {
       var $label = $("label[for='"+this.id+"']");
-      if ($(this).val() && this.checkValidity()) {
+      if (this.checkValidity()) {
         $(this).removeClass('-invalid');
         $label.removeClass('-invalid');
         $label.find('span.error_msg').remove();
