8f653b81ed2f85f75676ca8fc8c4d9584f93a49b
Michael Walchhütter <michael.walchhuetter@gmail.com>
2021-07-15 15:45:00 +0200

    started address api change ..

--- app/assets/javascripts/components/addressSearchAutocomplete.js
+++ app/assets/javascripts/components/addressSearchAutocomplete.js
@@ -1,10 +1,16 @@
 //= require typeahead.bundle
 
 APP.components.addressSearchAutocomplete = function() {
+  var region = $('body').data('region');
   var container = $("#addressSearchAutocomplete");
   if (container.length == 0) return;
 
-  var SEARCH_URL = 'https://data.wien.gv.at/daten/OGDAddressService.svc/GetAddressInfo?crs=EPSG:4326&Address=';
+  if (region == 'wien') {
+    var SEARCH_URL = 'https://data.wien.gv.at/daten/OGDAddressService.svc/GetAddressInfo?crs=EPSG:4326&Address=';
+  } else {
+    var SEARCH_URL = 'http://api.opendata.host/1.0/address/find?country=at&offset=1&limit=100&street-address=';
+  }
+
   var addressSearch = new Bloodhound({
     datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
     queryTokenizer: Bloodhound.tokenizers.whitespace,
@@ -14,7 +20,15 @@ APP.components.addressSearchAutocomplete = function() {
       filter: function(response) {
         selectAddress(response.features[0]);
         return response.features;
-      }
+      },
+      /*
+      ajax: {
+            beforeSend: function(jqXHR, settings) {
+               jqXHR.setRequestHeader("Access-Control-Allow-Credentials", 'true');
+               jqXHR.setRequestHeader('Authorization', 'Basic ' + btoa('9D46-38C6-31CF-40E9-BBBF-AFF6-37A3-D7C7:'));
+           }
+       }
+       */
     }
   });
   addressSearch.initialize();
