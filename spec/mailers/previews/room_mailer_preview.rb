e0b46c8d67ca98f34b0e10133325f1c3625f68d2
Michael Walchhütter <michael.walchhuetter@gmail.com>
2021-07-28 16:42:22 +0200

    fixed hosts for some mailers

--- spec/mailers/previews/room_mailer_preview.rb
+++ spec/mailers/previews/room_mailer_preview.rb
@@ -8,6 +8,14 @@ class RoomMailerPreview < ActionMailer::Preview
     RoomMailer.room_demand_published(RoomDemand.last)
   end
 
+  def room_offer_info_mail
+    RoomMailer.room_offer_info_mail(RoomOffer.last)
+  end
+
+  def room_demand_info_mail
+    RoomMailer.room_demand_info_mail(RoomDemand.last)
+  end
+
   def waiting_list_updated
     RoomMailer.waiting_list_updated(RoomOffer.last, User.last)
   end
