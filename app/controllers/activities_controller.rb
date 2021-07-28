0f989a1b95c74f092a0387beb10549388dbd81b1
Michael Walchhütter <michael.walchhuetter@gmail.com>
2021-07-28 10:18:48 +0200

    temporary fix for region stream filter

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,7 +4,8 @@ class ActivitiesController < ApplicationController
     head :ok and return if browser.bot? && !request.format.js?
     @graetzl = Graetzl.find(params[:graetzl_id]) if params[:graetzl_id]
     @district = District.find(params[:district_id]) if params[:district_id]
-    stream = ActivityStream.new(@graetzl, @district, current_user)
+    @region = Region.get(params[:region_id]) if params[:region_id]
+    stream = ActivityStream.new(@graetzl, @district, @region, current_user)
     @activity = stream.fetch.page(params[:page]).per(12)
     @activity_with_zuckerls = stream.insert_zuckerls(@activity) if params[:page].blank?
   end
