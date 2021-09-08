000edaec690925449e714f7c320c08bd0ebdd402
Michael Walchhütter <michael.walchhuetter@gmail.com>
2021-09-08 17:57:46 +0200

    district graetzl select fix

--- app/assets/javascripts/components/district_graetzl_input.js
+++ app/assets/javascripts/components/district_graetzl_input.js
@@ -2,6 +2,26 @@ APP.components.districtGraetzlInput = function() {
 
   var districtInput = $(".district-input");
   if (districtInput.length == 0) return;
+
+  var graetzlInput = $(".graetzl-input");
+  var graetzlInputSource = graetzlInput.clone();
+
+  districtInput.on('change', function() {
+    var districtId = $(this).val();
+    if (!districtId) return;
+
+    graetzlInput.find("option").remove();
+    graetzlInput.append(graetzlInputSource.find("[data-district-id='" + districtId + "']").clone());
+
+  }).trigger("change");
+
+};
+
+/*
+APP.components.districtGraetzlInput = function() {
+
+  var districtInput = $(".district-input");
+  if (districtInput.length == 0) return;
   var graetzlInput = $(".graetzl-input");
 
   districtInput.on('change', function() {
@@ -16,3 +36,4 @@ APP.components.districtGraetzlInput = function() {
   }).trigger("change");
 
 };
+*/
