6789a074366077ffb3520ad30e1abb5e86e48264
Nikola Šantić <anrakee@gmail.com>
2021-10-03 15:42:25 +0200

    Fixed wien redirects

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -3,9 +3,10 @@ class RedirectController < ApplicationController
   def wien
     if params[:wien_path].blank?
       redirect_to region_index_url
-    elsif params[:wien_path].start_with?('raumteiler')
-      # wien/raumteiler/raumsuche, wien/raumteiler/raum/, wien/raumteiler/open-calls
+    elsif ['raumteiler/raumsuche', 'raumteiler/raum', 'raumteiler/open-calls'].any?{|p| params[:wien_path].start_with?(p)}
       redirect_to params[:wien_path].delete_prefix('raumteiler')
+    elsif ['raumteiler', 'toolteiler', 'gruppen', 'locations', 'treffen', 'zuckerl'].any?{|p| params[:wien_path].start_with?(p)}
+      redirect_to "/region/" + params[:wien_path]
     elsif current_region.districts.any?{|d| params[:wien_path].start_with?(d.slug) }
       redirect_to "/bezirk/#{params[:wien_path]}"
     else
