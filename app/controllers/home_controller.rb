ddbbd6b38540120d412165eb9cd4fd4d91ad7c0c
Nikola Šantić <anrakee@gmail.com>
2021-10-03 14:25:37 +0200

    No longer using mail image helper; saving last used region in cookie

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,13 @@ class HomeController < ApplicationController
   def index
     if current_region.nil?
       region = find_best_region
-      redirect_to region ? root_url(host: region.host) : about_platform_url and return
+      redirect_to region ? region_root_url(region) : about_platform_url and return
     end
 
     if current_user && user_home_graetzl
       redirect_to user_home_graetzl
     else
+      remember_region if !current_user
       @activity_sample = ActivitySample.new(current_region: current_region)
       render 'region'
     end
@@ -23,6 +24,8 @@ class HomeController < ApplicationController
   def find_best_region
     if current_user
       current_user.region
+    elsif session[:region_id].present?
+      Region.get(session[:region_id])
     else
       IpResolver.new.find_region(request.remote_ip)
     end
