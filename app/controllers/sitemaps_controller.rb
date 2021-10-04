c70850c810f5ad192279597074e021d1acde6095
Michael Walchhütter <michael.walchhuetter@gmail.com>
2021-10-04 20:51:02 +0200

    prepared sitemaps

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -2,7 +2,9 @@ class SitemapsController < ApplicationController
 
   def sitemap
 
-    redirect_to "https://s3.eu-central-1.amazonaws.com/im-graetzl-production/sitemaps/#{current_region.id}/sitemap.xml.gz"
+    if !current_region.nil?
+      redirect_to "https://s3.eu-central-1.amazonaws.com/im-graetzl-production/sitemaps/#{current_region.id}/sitemap.xml.gz"
+    end
 
   end
 
