b51c5ae207e3b698f96ac4cbd64f73ba10378adb
Michael Walchhütter <michael.walchhuetter@gmail.com>
2021-12-14 14:34:05 +0100

    Revert "Revert "try Dynamic Rendering for seo googlebot on special pages (cardbox overviews)""
    This reverts commit 7c21a8267c3b7c2d4d0ae98cbc37292fe4b8d3c2.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  include DynamicRendering::ActsAsDynamicallyRenderable if Rails.env.development? || Rails.env.test? || Rails.env.staging?
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
