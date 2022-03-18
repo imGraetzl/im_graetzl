dd9d051a9402d483b0e8ec41ca277f1dd75763f9
Michael Walchhütter <michael.walchhuetter@gmail.com>
2022-03-18 13:05:05 +0100

    insert edit permissions depended on status

--- app/helpers/upload_helper.rb
+++ app/helpers/upload_helper.rb
@@ -1,10 +1,10 @@
 module UploadHelper
 
-  def upload_image_input(f, field_name, multiple: false, live_preview: true)
+  def upload_image_input(f, field_name, multiple: false, live_preview: true, disabled:false)
     content_tag(:div, class: 'upload-container') do
       concat f.file_field(field_name, multiple: multiple, class: 'direct-upload-input',
         accept: ImageUploader::ALLOWED_TYPES.join(","),
-        data: { upload_server: Rails.application.config.upload_server }
+        data: { upload_server: Rails.application.config.upload_server }, disabled: disabled
       )
 
       if multiple
@@ -24,7 +24,7 @@ module UploadHelper
 
       if f.object.public_send(field_name).present? || live_preview
         concat(content_tag(:div, class: "upload-previews #{field_name} #{f.object.class.name.downcase} #{f.object.public_send(field_name).present? ? 'show-hint' : ''}") do
-          multiple ? uploaded_images_edit(f, field_name) : uploaded_image_edit(f, field_name)
+          multiple ? uploaded_images_edit(f, field_name, disabled:disabled) : uploaded_image_edit(f, field_name, disabled:disabled)
         end)
         if field_name.to_s == 'cover_photo'
           concat(content_tag(:small, 'Info: Dein Bild wird wie oberhalb dargestellt zugeschnitten. Wähle ein längliches Bild-Format (ca. 980 x 400 Pixel) sollte es nicht passen.', class: 'img-upload-hint'))
@@ -36,24 +36,24 @@ module UploadHelper
     end
   end
 
-  def uploaded_image_edit(f, field_name)
+  def uploaded_image_edit(f, field_name, disabled:false)
     return if !f.object.public_send(field_name)
     content_tag(:div, class: 'upload-preview') do
       concat image_tag(f.object.public_send(field_name).url, class: 'upload-preview-image')
       concat(content_tag(:div, class: 'input-checkbox') do
-        f.check_box(:"remove_#{field_name}", class: 'deleteCheckbox') +
+        f.check_box(:"remove_#{field_name}", class: 'deleteCheckbox', disabled: disabled) +
         f.label(:"remove_#{field_name}", 'Löschen')
       end)
     end
   end
 
-  def uploaded_images_edit(f, field_name)
+  def uploaded_images_edit(f, field_name, disabled:false)
     f.fields_for(field_name) do |ff|
       content_tag(:div, class: 'upload-preview') do
         concat image_tag(ff.object.file.url, class: 'upload-preview-image')
         concat ff.hidden_field(:file, value: ff.object.cached_file_data) if ff.object.new_record?
         concat(content_tag(:div, class: 'input-checkbox') do
-          ff.check_box(:"_destroy", class: 'deleteCheckbox') +
+          ff.check_box(:"_destroy", class: 'deleteCheckbox', disabled: disabled) +
           ff.label(:_destroy, 'Löschen')
         end)
       end
