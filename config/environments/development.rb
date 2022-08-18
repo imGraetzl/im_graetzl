4460749d80fee4164d35de9218f2beb8654bb350
Michael Walchhütter <michael.walchhuetter@gmail.com>
2022-08-18 22:06:53 +0200

    try asset_host config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,10 @@ Rails.application.configure do
   config.hosts << config.imgraetzl_host
   config.hosts << ".#{config.welocally_host}"
 
+  config.action_controller.asset_host = Proc.new { |source, request|
+    "#{request.protocol}#{request.host_with_port}"
+  }
+
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
