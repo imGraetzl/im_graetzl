5c418c9f209fac5e3b56618ea0d39984cd361113
Michael Walchhütter <michael.walchhuetter@gmail.com>
2023-08-31 11:02:55 +0200

    export pololusers csv

--- app/admin/poll_user.rb
+++ app/admin/poll_user.rb
@@ -9,4 +9,23 @@ ActiveAdmin.register PollUser do
   index { render 'index', context: self }
   show { render 'show', context: self }
 
+  controller do
+    def apply_pagination(chain)
+        chain = super unless formats.include?(:json) || formats.include?(:csv)
+        chain
+    end
+  end
+
+  csv do
+    column :id
+    column(:plz) {|poll_user| poll_user.poll.zip }
+    column :poll
+    column(:name)   {|poll_user| poll_user.user.full_name}
+    column(:email)  {|poll_user| poll_user.user.email}
+
+    column(:poll_user_answers) {|poll_user| poll_user.poll_user_answers.map(&:poll_option).join(", ") }
+    column(:free_answers) {|poll_user| poll_user.poll_user_answers.map(&:answer).join(", ") }
+    column :created_at
+  end
+
 end
