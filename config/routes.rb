15d7ed80a897a5aaf6cf8771e801a9ac5102fcff
Michael Walchhütter <michael.walchhuetter@gmail.com>
2023-09-19 22:02:17 +0200

    insert redirect

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   get  'home' => 'home#about', as: 'about_platform'
   post 'geolocation'  => 'home#geolocation'
 
-  get 'https://www.imgraetzl.at/crowdfunding/osterreichische-hard-iced-teas-cassy-und-marty-sind-bereit-fur-deine-party' => redirect('https://graz.welocally.at/crowdfunding/osterreichische-hard-iced-teas-cassy-und-marty-sind-bereit-fur-deine-party')
+  get 'crowdfunding/osterreichische-hard-iced-teas-cassy-und-marty-sind-bereit-fur-deine-party' => redirect('https://graz.welocally.at/crowdfunding/oesterreichische-hard-iced-teas-cassy-und-marty-sind-bereit-fur-deine-party')
   get 'call-2022' => redirect('andocken')
   get 'andocken' => 'region_calls#call'
   post 'andocken' => 'region_calls#create'
