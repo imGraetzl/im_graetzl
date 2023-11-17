0e6bde4c3ffef28626ac9fbd389b1d35b3483930
Michael Walchhütter <michael.walchhuetter@gmail.com>
2023-11-17 15:05:26 +0100

    mailfix

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -7,7 +7,7 @@ class AdminMailer < ApplicationMailer
       subject: "[#{@region.host_domain_name}] Daily Mail",
       from: platform_email("no-reply"),
       to: platform_email("wir"),
-      cc: "lena@imgraetzl.com, carina@imgraetzl.at"
+      cc: "lena@imgraetzl.at, carina@imgraetzl.at"
     )
   end
 
