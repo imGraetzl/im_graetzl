172bfa561d00010aab0e798d01f25fcc5db0ae21
Michael Walchhütter <michael.walchhuetter@gmail.com>
2024-01-05 11:02:58 +0100

    added csp frame_ancestors to self

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,14 +4,8 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-#Rails.application.configure do
-#  config.content_security_policy do |policy|
-#    #policy.style_src :self, :unsafe_inline
-#    policy.style_src :self, :unsafe_inline, "*.welocally.at", "*.stripe.com"
-#  end
-#end
-
-# Rails.application.config.content_security_policy do |policy|
+Rails.application.config.content_security_policy do |policy|
+    policy.frame_ancestors :self
 #   policy.default_src :self, :https
 #   policy.font_src    :self, :https, :data
 #   policy.img_src     :self, :https, :data
@@ -21,7 +15,7 @@
 
 #   # Specify URI for violation reports
 #   # policy.report_uri "/csp-violation-report-endpoint"
-# end
+end
 
 # If you are using UJS then enable automatic nonce generation
 # Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
