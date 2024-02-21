2bcf4f5a598b3496d112addb8495fdc359b92524
Michael Walchhütter <michael.walchhuetter@gmail.com>
2024-02-21 14:50:29 +0100

    small search fix

--- app/assets/javascripts/components-loggedin/search.js
+++ app/assets/javascripts/components-loggedin/search.js
@@ -62,7 +62,7 @@ APP.components.search = (function() {
       ],
       list: {
         //match: {enabled: true}, // searchphrase must be in shown result
-        maxNumberOfElements: 10,
+        maxNumberOfElements: 16,
         onShowListEvent:function() {
           search_phrase = $input.val();
           addCategoryLinks();
