e3028c9f3adce00e50dda2f9dcf273947ba0942d
Michael Walchhütter <michael.walchhuetter@gmail.com>
2024-03-29 14:21:05 +0100

    Add Linz Region Icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,7 +20,7 @@ module ApplicationHelper
   end
 
   def icon_tag(name, options = {})
-    "<svg class='icon-#{name} icon #{options[:class]}'><use xlink:href='/assets/icons_010324.svg#icon-#{name}'></use></svg>".html_safe
+    "<svg class='icon-#{name} icon #{options[:class]}'><use xlink:href='/assets/icons_010424.svg#icon-#{name}'></use></svg>".html_safe
   end
 
   def icon_with_badge(icon_name, number, options = {})
