b0453f5592f3e6235dcfc003c2accd443071d7a5
Michael Walchhütter <michael.walchhuetter@gmail.com>
2024-07-19 20:40:50 +0200

    rollback notification admin view

--- app/admin/notifications.rb
+++ app/admin/notifications.rb
@@ -4,18 +4,18 @@ ActiveAdmin.register Notification, as: "Notifications" do
 
   #scope :ready_to_be_sent
   scope :all
-  scope 'Next Newsletter', :next_newsletter, default: true
-  #scope 'Next Wien Mails', :next_wien
-  #scope 'Next Graz Mails', :next_graz
-  #scope 'Next Linz Mails', :next_linz
-  #scope 'Next Kärnten Mails',:next_kaernten
-  #scope 'Next Mühlviertel Mails',:next_muehlviertel
+  #scope 'Next Newsletter', :next_newsletter, default: true
+  scope 'Next Wien Mails', :next_wien, default: true
+  scope 'Next Graz Mails', :next_graz
+  scope 'Next Linz Mails', :next_linz
+  scope 'Next Kärnten Mails',:next_kaernten
+  scope 'Next Mühlviertel Mails',:next_muehlviertel
 
-  filter :user, label: 'Next Region Newsletter', collection: proc { User.admin_newsletter_collection }, include_blank: true, input_html: { class: 'admin-filter-select'}
+  #filter :user, label: 'Next Region Newsletter', collection: proc { User.admin_newsletter_collection }, include_blank: true, input_html: { class: 'admin-filter-select'}
   filter :type, as: :select, include_blank: true, input_html: { class: 'admin-filter-select'}
   filter :sent, include_blank: true, input_html: { class: 'admin-filter-select'}
   filter :seen, include_blank: true, input_html: { class: 'admin-filter-select'}
-  #filter :region_id, label: 'Region', as: :select, collection: proc { Region.all }, include_blank: true, input_html: { class: 'admin-filter-select'}
+  filter :region_id, label: 'Region', as: :select, collection: proc { Region.all }, include_blank: true, input_html: { class: 'admin-filter-select'}
   #filter :user_id, :as => :numeric
   filter :notify_at
   filter :notify_before
