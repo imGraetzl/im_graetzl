ad2a7af38d343cfc0cb7403c4a826a36c978e0f9
Michael Walchhütter <michael.walchhuetter@gmail.com>
2024-09-19 11:31:37 +0200

    add logger to unseen count, investigate requests

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,7 +4,9 @@ class NotificationsController < ApplicationController
   NOTIFICATIONS_PER_PAGE = 6
 
   def unseen_count
-    render json: { count: current_user.new_website_notifications_count }
+    Rails.logger.info("[Request Unseen Count for User: #{current_user.id}]")
+    render json: { count: 0 }
+    #render json: { count: current_user.new_website_notifications_count }
   end
 
   def fetch
