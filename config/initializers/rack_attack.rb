0deff699ecd49516446a233e0c9c89204bc1794b
Michael Walchhütter <michael.walchhuetter@gmail.com>
2024-10-11 15:35:49 +0200

    improved rack attack

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -2,13 +2,13 @@ class Rack::Attack
 
   Rack::Attack.blocklisted_responder = lambda do |req|
     # Log blocked request
-    Rails.logger.info "[Blocked request: #{req.ip} for path #{req.path}]"
+    Rails.logger.info "[403 Forbidden | Blocked request: #{req.ip} for path #{req.path}]"
     # Optionally send a custom response
-    [503, { 'Content-Type' => 'text/plain' }, ['Blocked']]
+    [403, { 'Content-Type' => 'text/plain' }, ['403 Forbidden']]
   end
 
   blocklist('block script kiddies') do |req|
-    req.path =~ /\.(php|asp|aspx|jsp|cgi|exe)$/i ||
+    req.path =~ /\.(php|asp|aspx|jsp|cgi|exe|jsf)$/i ||
     req.path&.start_with?('/wp-content')
   end
 
