9d396de80a184d25fdd35fe5df321386fcbe994f
Michael Walchhütter <michael.walchhuetter@gmail.com>
2024-11-08 20:52:12 +0100

    logging and email validation

--- app/models/crowd_donation_pledge.rb
+++ app/models/crowd_donation_pledge.rb
@@ -9,6 +9,7 @@ class CrowdDonationPledge < ApplicationRecord
   before_create :set_region_and_type
   before_save :normalize_email
 
+  validates :email, format: { with: Devise.email_regexp }
   validates_presence_of :email, :contact_name
 
   def email=(val)
