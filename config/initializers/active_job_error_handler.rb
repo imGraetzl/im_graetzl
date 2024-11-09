4aee4698a1051454ddbbd47f27502cc0550149bc
Michael Walchhütter <michael.walchhuetter@gmail.com>
2024-11-09 22:56:33 +0100

    try delayed job rescue error handling

---
+++ config/initializers/active_job_error_handler.rb
@@ -0,0 +1,16 @@
+# Error-Handling während der Deserialisierung:
+# Erweiterung der Delayed::PsychExt-Klasse, die für die Deserialisierung von Jobs zuständig ist.
+# Bei ActiveRecord::RecordNotFound, wird er geloggt, und der Job wird verworfen (nil zurückgegeben).
+
+require 'delayed/psych_ext'
+
+module Delayed
+  module PsychExt
+    def visit_Psych_Nodes_Mapping(object)
+      super
+    rescue ActiveRecord::RecordNotFound => e
+      Rails.logger.error("Fehler bei Delayed Job Deserialisierung: #{e.message}. Job wird übersprungen.")
+      nil # Gibt `nil` zurück, um den Job als fehlgeschlagen zu markieren und nicht erneut auszuführen
+    end
+  end
+end
