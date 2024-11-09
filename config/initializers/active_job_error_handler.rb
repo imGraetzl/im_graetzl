9d964a6f5f57489dd6ebf4650aa90b92b8ea57d9
Michael Walchhütter <michael.walchhuetter@gmail.com>
2024-11-09 23:22:10 +0100

    new try error handling worker crash

--- config/initializers/active_job_error_handler.rb
+++ config/initializers/active_job_error_handler.rb
@@ -1,9 +1,17 @@
 # config/initializers/active_job_error_handler.rb
+
+Rails.logger.info("Active Job Error Handler Initializer geladen")
+
 Delayed::Worker.lifecycle.around(:error) do |job, &block|
   begin
     block.call
+  rescue Delayed::DeserializationError => e
+    Rails.logger.error("Globaler DeserializationError-Handler: #{e.message}. Job wird übersprungen.")
+    # Überspringen, um Neustarts zu verhindern.
+    nil
   rescue ActiveRecord::RecordNotFound => e
-    Rails.logger.error("Globaler Error-Handler: #{e.message}. Job wird übersprungen.")
-    nil # Verhindert erneute Versuche und Crash bei RecordNotFound-Fehlern
+    Rails.logger.error("Globaler RecordNotFound-Handler: #{e.message}. Job wird übersprungen.")
+    # Weitere Behandlung hinzufügen falls nötig
+    nil
   end
 end
