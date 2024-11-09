626ed44a021e780375b7468f1adda4499a033d7a
Michael Walchhütter <michael.walchhuetter@gmail.com>
2024-11-09 23:11:20 +0100

    new crash avaoding

--- config/initializers/active_job_error_handler.rb
+++ config/initializers/active_job_error_handler.rb
@@ -1,17 +1,9 @@
-# Error-Handling während der Deserialisierung:
-# Erweiterung der Delayed::PsychExt-Klasse, die für die Deserialisierung von Jobs zuständig ist.
-# Bei ActiveRecord::RecordNotFound, wird er geloggt, und der Job wird verworfen (nil zurückgegeben).
-
-require 'delayed/psych_ext'
-Rails.logger.info("Active Job Error Handler Initializer geladen")
-
-module Delayed
-  module PsychExt
-    def visit_Psych_Nodes_Mapping(object)
-      super
-    rescue ActiveRecord::RecordNotFound => e
-      Rails.logger.error("Fehler bei Delayed Job Deserialisierung: #{e.message}. Job wird übersprungen.")
-      nil # Gibt `nil` zurück, um den Job als fehlgeschlagen zu markieren und nicht erneut auszuführen
-    end
+# config/initializers/active_job_error_handler.rb
+Delayed::Worker.lifecycle.around(:error) do |job, &block|
+  begin
+    block.call
+  rescue ActiveRecord::RecordNotFound => e
+    Rails.logger.error("Globaler Error-Handler: #{e.message}. Job wird übersprungen.")
+    nil # Verhindert erneute Versuche und Crash bei RecordNotFound-Fehlern
   end
 end
