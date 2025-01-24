0aaf23dbf7aa4ebd96ca7ec8b411b1882d075b3e
Michael Walchhütter <michael.walchhuetter@gmail.com>
2025-01-24 21:43:53 +0100

    add scopes

--- app/models/coupon_history.rb
+++ app/models/coupon_history.rb
@@ -2,6 +2,8 @@ class CouponHistory < ApplicationRecord
   belongs_to :user
   belongs_to :coupon, optional: true    # Referenz auf Coupon, kann leer sein
 
+  scope :valid, -> { where('valid_until >= ?', Time.current) }
+  scope :expired, -> { where('valid_until < ?', Time.current) }
   scope :redeemed, -> { where.not(redeemed_at: nil) }
 
 end
