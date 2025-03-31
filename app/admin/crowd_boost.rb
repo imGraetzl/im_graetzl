7f56c83db7e13cefc2b3d65f2b59004efa53c699
Michael Walchhütter <michael.walchhuetter@gmail.com>
2025-03-31 12:35:02 +0200

    reverted admin stuff for url fix

--- app/admin/crowd_boost.rb
+++ app/admin/crowd_boost.rb
@@ -14,17 +14,6 @@ ActiveAdmin.register CrowdBoost do
   permit_params :status, :pledge_charge, :slug, :title, :slogan, :description, :avatar, :remove_avatar, :chargeable_status, :charge_description, :region_ids => []
 
   controller do
-    def find_resource
-      CrowdBoost.find(params[:id])
-    end
-    before_action do
-      # Monkeypatch: temporär to_param für alle CrowdBoost-Instanzen im Admin
-      CrowdBoost.class_eval do
-        define_method(:to_param) do
-          id.to_s
-        end
-      end
-    end
     def apply_pagination(chain)
       chain = super unless formats.include?(:json) || formats.include?(:csv)
       chain
