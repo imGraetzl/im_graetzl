346cf4cb76c50b667464d0669df8be7ab7f60fab
Michael Walchhütter <michael.walchhuetter@gmail.com>
2025-04-15 10:45:40 +0200

    new logging for strange img requests

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,11 +1,6 @@
 class ErrorsController < ApplicationController
 
   def not_found
-
-    if request.path.include?("/attachments/") && request.path.include?("/fill/")
-      Rails.logger.warn("[404 IMAGE] path=#{request.path} | ip=#{request.remote_ip} | ua=#{request.user_agent} | referer=#{request.referer}")
-    end
-
     respond_to do |format|
       format.html { render 'errors/not_found', status: 404 }
       format.all { head 404 }
@@ -17,6 +12,11 @@ class ErrorsController < ApplicationController
       format.html { render 'errors/internal_server_error', status: 500 }
       format.all { head 500 }
     end
+  end
+
+  def missing_attachment_logger
+    Rails.logger.warn("[MISSING THUMBNAIL] #{request.fullpath} | ip=#{request.remote_ip} | ua=#{request.user_agent} | referer=#{request.referer}")
+    head :not_found
   end
 
 end
