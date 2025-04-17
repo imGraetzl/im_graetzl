5ecc187fa76088479653125503827c58ad498e44
Michael Walchhütter <michael.walchhuetter@gmail.com>
2025-04-17 11:53:39 +0200

    small fix

--- app/services/room_booster_service.rb
+++ app/services/room_booster_service.rb
@@ -65,7 +65,7 @@ class RoomBoosterService
 
   def payment_refunded(room_booster)
     room_booster.update(payment_status: 'refunded', status: 'storno')
-    ActionProcessor.track(room_booster.room_offer, :boost_refund, room_booster) # Raumteiler Activity Reset
+    ActionProcessor.track(room_booster.room_offer, :boost_refund, room_booster) if room_booster.room_offer
     true
   end
 
