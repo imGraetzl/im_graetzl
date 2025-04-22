c59eed4958045b0f81b141788bdf8f21a9d5cc7e
Michael Walchhütter <michael.walchhuetter@gmail.com>
2025-04-22 21:24:56 +0200

    improved rack attack

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -8,7 +8,7 @@ class Rack::Attack
   end
 
   blocklist('block script kiddies') do |req|
-    req.path =~ /\.(php|asp|aspx|jsp|cgi|exe|jsf)$/i ||
+    req.path =~ /\.(php|asp|aspx|jsp|cgi|exe|jsf|pl|py|sh|cfm)$/i ||
     req.path&.start_with?('/wp-') ||
     req.path.include?('.git')
   end
@@ -21,7 +21,7 @@ class Rack::Attack
   # **BLOCKLIST: Bestimmte IP-Adressen**
   blocklist('block specific IPs') do |req|
     blocked_ips = [
-      '103.121.39.54', '185.208.8.76'  # Scanner/Bot
+      '103.121.39.54', '185.208.8.76', '139.59.245.198'  # Scanner/Bot
     ]
     blocked_ips.include?(req.ip)
   end
