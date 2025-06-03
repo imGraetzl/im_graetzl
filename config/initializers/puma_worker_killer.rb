2d1a6a9456c02aee7a6c56c3da37baaada112fbe
Michael Walchhütter <michael.walchhuetter@gmail.com>
2025-06-03 14:08:08 +0200

    fixed PumaWorkerKiller setup

--- config/initializers/puma_worker_killer.rb
+++ config/initializers/puma_worker_killer.rb
@@ -1,23 +1,9 @@
 PumaWorkerKiller.config do |config|
-  # dyno memory quota detection
-  case `bash -c 'ulimit -u'`.to_i
-  when 256
-    # 1X
-    config.ram = 512
-  when 512
-    # 2X
-    config.ram = 1024
-  when 16_384
-    # Performance-M
-    config.ram = 2_560
-  else
-    # Performance-L or unknown
-    config.ram = 14_336
-  end
+  config.ram = (ENV['DYNO_RAM_MB'] || 1024).to_i
   config.frequency = 60 # seconds
   config.percent_usage = (ENV['MAX_MEMORY_USAGE'] || 0.94).to_f
   config.rolling_restart_frequency = false
-  Rails.logger.warn "Puma worker killer configured with #{config.ram} MB of RAM (#{config.percent_usage * 100}%)"
+  Rails.logger.info "Puma worker killer configured with #{config.ram} MB of RAM (#{config.percent_usage * 100}%)"
 end
 
 PumaWorkerKiller.start
