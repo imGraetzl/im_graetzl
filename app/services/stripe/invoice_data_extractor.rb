4fc13e8e7364b62227894ff7e32287fe3d6f0bc1
Michael Walchhütter <michael.walchhuetter@gmail.com>
2025-06-12 11:59:37 +0200

    fix

--- app/services/stripe/invoice_data_extractor.rb
+++ app/services/stripe/invoice_data_extractor.rb
@@ -5,21 +5,21 @@ module Stripe
     end
 
     def subscription_id
-      @invoice.subscription ||
-        @invoice.parent&.subscription_details&.subscription ||
-        @invoice.lines&.data&.first&.parent&.subscription_item_details&.subscription
+      @invoice[:subscription] ||
+        @invoice[:parent]&.[](:subscription_details)&.[](:subscription) ||
+        @invoice[:lines]&.[](:data)&.first&.[](:parent)&.[](:subscription_item_details)&.[](:subscription)
     end
 
     def period_start
-      @invoice.lines&.data&.first&.period&.start
+      @invoice[:lines]&.[](:data)&.first&.[](:period)&.[](:start)
     end
 
     def period_end
-      @invoice.lines&.data&.first&.period&.end
+      @invoice[:lines]&.[](:data)&.first&.[](:period)&.[](:end)
     end
 
     def amount_eur
-      (@invoice.amount_remaining || @invoice.amount_due || 0).to_f / 100.0
+      (@invoice[:amount_remaining] || @invoice[:amount_due] || 0).to_f / 100.0
     end
   end
 end
