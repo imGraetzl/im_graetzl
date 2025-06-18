0c5b2824995d4f588073cf9dc2590246a48d0b73
Michael Walchhütter <michael.walchhuetter@gmail.com>
2025-06-18 13:08:50 +0200

    remove accepts_nested_attributes_for :contact

--- app/models/location.rb
+++ app/models/location.rb
@@ -24,8 +24,6 @@ class Location < ApplicationRecord
   has_many :comments, as: :commentable, dependent: :destroy
   has_many :favorites, as: :favoritable, dependent: :destroy
 
-  accepts_nested_attributes_for :contact
-
   has_many :meetings
   has_many :upcoming_meetings, -> { upcoming }, class_name: "Meeting"
   has_one  :next_upcoming_meeting, -> { upcoming.order(:starts_at_date) }, class_name: "Meeting"
