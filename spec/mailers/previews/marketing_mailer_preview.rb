afe2df4f837db1147e9eeb006d636a829ed7e95e
Michael Walchhütter <michael.walchhuetter@gmail.com>
2025-06-23 12:19:24 +0200

    cleanup

--- spec/mailers/previews/marketing_mailer_preview.rb
+++ spec/mailers/previews/marketing_mailer_preview.rb
@@ -8,8 +8,4 @@ class MarketingMailerPreview < ActionMailer::Preview
     MarketingMailer.agb_change_and_crowdfunding(User.last)
   end
 
-  def beat_1060
-    MarketingMailer.beat_1060(User.last)
-  end
-
 end
