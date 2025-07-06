832bbe57e76a303a2148f6b065c9dd8d152057cc
Michael Walchhütter <michael.walchhuetter@gmail.com>
2025-07-06 17:26:34 +0200

    sitemap redirect fix

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -1,13 +1,9 @@
 class SitemapsController < ApplicationController
-
   def sitemap
-
     if current_region.nil?
-      redirect_to "https://s3.eu-central-1.amazonaws.com/#{ENV['UPLOADS_BUCKET']}/sitemaps/welocally/sitemap.xml.gz"
+      redirect_to "https://s3.eu-central-1.amazonaws.com/#{ENV['UPLOADS_BUCKET']}/sitemaps/welocally/sitemap.xml.gz", allow_other_host: true
     else
-      redirect_to "https://s3.eu-central-1.amazonaws.com/#{ENV['UPLOADS_BUCKET']}/sitemaps/#{current_region.id}/sitemap.xml.gz"
+      redirect_to "https://s3.eu-central-1.amazonaws.com/#{ENV['UPLOADS_BUCKET']}/sitemaps/#{current_region.id}/sitemap.xml.gz", allow_other_host: true
     end
-
   end
-
 end
