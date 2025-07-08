a8bf98bff805ba184e506319d5813195b8976eee
Michael Walchhütter <michael.walchhuetter@gmail.com>
2025-07-08 22:15:22 +0200

    moved icon sprite back to public assets, no cdn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,8 +32,7 @@ module ApplicationHelper
   end
 
   def icon_tag(name, options = {})
-    sprite_url = '/assets/svg/icon_sprite.svg'
-    "<svg class='icon-#{name} icon #{options[:class]}'><use xlink:href='#{sprite_url}#icon-#{name}'></use></svg>".html_safe
+    "<svg class='icon-#{name} icon #{options[:class]}'><use xlink:href='/assets/icons_220125.svg#icon-#{name}'></use></svg>".html_safe
   end
 
   def icon_with_badge(icon_name, number, options = {})
