e2996b4c616a5422dcb72357c20f6f7612b2bf1a
Michael Walchhütter <michael.walchhuetter@gmail.com>
2025-09-15 19:22:25 +0200

    small fixes

--- app/controllers/subscription_plans_controller.rb
+++ app/controllers/subscription_plans_controller.rb
@@ -3,6 +3,7 @@ class SubscriptionPlansController < ApplicationController
   def show
     @plans = SubscriptionPlan.enabled.in(current_region).all
     @subscriptions = Subscription.in(current_region).active.includes(user: :graetzl).order(created_at: :desc)
+    @subscription_count = @subscriptions.size
   end
 
 end
