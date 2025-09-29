7268c79f0f68ea8ab718b3c1ffdc05196a5a332d
Michael Walchhütter <michael.walchhuetter@gmail.com>
2025-09-30 00:10:12 +0200

    fix loggers

--- config/initializers/delayed_job_logger.rb
+++ config/initializers/delayed_job_logger.rb
@@ -1,5 +1,14 @@
+# frozen_string_literal: true
+
 if Rails.env.production? || Rails.env.staging?
   if defined?(Delayed::Worker)
-    Delayed::Worker.logger.level = Logger::WARN
+    # Eigener Logger nur für Delayed Job
+    dj_logger = ActiveSupport::Logger.new($stdout)
+    dj_logger.formatter = Rails.application.config.log_formatter
+    dj_logger.level = Logger::WARN
+
+    Delayed::Worker.logger = dj_logger
+
+    Rails.logger.info "[DelayedJobLogger] Initialized separate DJ logger with level WARN"
   end
 end
