cf77828b9f80ef74de4bbef2561cccf66bb44977
Michael Walchhütter <michael.walchhuetter@gmail.com>
2025-11-19 13:47:05 +0100

    seeds bereinigt

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
-require_relative "seeds/bookables"
-
-def invoke_task_if_exists(task_name)
-  return unless Rake::Task.task_defined?(task_name)
-
-  Rake::Task[task_name].invoke
+# -----------------------------------------
+#   Seeds nur in Development & Staging
+# -----------------------------------------
+unless Rails.env.production?
+  require_relative "seeds/bookables"
 end
-
-invoke_task_if_exists("db:import_districts")
-invoke_task_if_exists("db:import_graetzls")
