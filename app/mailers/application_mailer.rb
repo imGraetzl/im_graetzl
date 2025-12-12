8b015982902e1df4b0e2ea855f82f2512c5dfbdd
Michael Walchhütter <michael.walchhuetter@gmail.com>
2025-12-12 13:17:45 +0100

    fixed mail links

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -8,7 +8,11 @@ class ApplicationMailer < ActionMailer::Base
   end
 
   def default_url_options
-    Rails.application.default_url_options.merge(host: @region.host)
+    region = @region || Region.get('wien')
+    host = region.host
+    host = "www.#{host}" if region.id == 'wien' && !host.start_with?('www.')
+
+    Rails.application.default_url_options.merge(host: host)
   end
 
   def platform_email(email_name, label_name = nil)
