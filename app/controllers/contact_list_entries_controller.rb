9141fb375f1e0b2153a42c8627e33009b49c0332
Michael Walchhütter <michael.walchhuetter@gmail.com>
2026-01-31 21:07:51 +0100

    nomination closed

--- app/controllers/contact_list_entries_controller.rb
+++ app/controllers/contact_list_entries_controller.rb
@@ -2,14 +2,23 @@ class ContactListEntriesController < ApplicationController
   layout :set_layout
   rate_limit to: 25, within: 5.minutes, only: [:submit_sheboost]
   SHEBOOST_SLIDER_LIMIT = 2
+  SHEBOOST_TIME_ZONE = 'Europe/Vienna'
+  SHEBOOST_NOMINATION_END_AT = Time.find_zone(SHEBOOST_TIME_ZONE)
+    .parse(ENV.fetch('SHEBOOST_NOMINATION_END_AT', '2026-02-01 00:00'))
 
   def sheboost
     @contact_list_entry = ContactListEntry.new
     @sheboost_entries_limit = SHEBOOST_SLIDER_LIMIT
     @sheboost_entries = sheboost_entries_scope('/sheboost').limit(SHEBOOST_SLIDER_LIMIT)
+    @sheboost_nominations_closed = sheboost_nominations_closed?
   end
 
   def submit_sheboost
+    if sheboost_nominations_closed?
+      flash[:alert] = "Die Nominierungsphase ist beendet. Die sheBOOST Jury tagt nun.."
+      redirect_to sheboost_path
+      return
+    end
     @contact_list_entry = ContactListEntry.new(contact_list_entries_params)
     @contact_list_entry.region_id = current_region.id if current_region
     @contact_list_entry.via_path = request.path
@@ -21,6 +30,7 @@ class ContactListEntriesController < ApplicationController
     else
       @sheboost_entries_limit = SHEBOOST_SLIDER_LIMIT
       @sheboost_entries = sheboost_entries_scope('/sheboost').limit(SHEBOOST_SLIDER_LIMIT)
+      @sheboost_nominations_closed = sheboost_nominations_closed?
       render 'sheboost'
     end
   end
@@ -51,6 +61,10 @@ class ContactListEntriesController < ApplicationController
 
   def sheboost_entries_scope(via_path)
     ContactListEntry.where(via_path: via_path).order(created_at: :desc)
+  end
+
+  def sheboost_nominations_closed?
+    Time.find_zone(SHEBOOST_TIME_ZONE).now >= SHEBOOST_NOMINATION_END_AT
   end
 
   def set_layout
